35be5a52f7440657e60c40a5901624b1caa11c8f
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-30 19:50:31 +0100

    play around with a few db queries - will export them in separate files, this is just testing

--- server/app.js
+++ server/app.js
@@ -18,3 +18,51 @@ db.connect((err) => {
     });
 })
 
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM quotes';
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw {...err}
+        }
+        res.status(200).send(result);
+    })
+})
+
+app.get('/create-database', (req, res) => {
+    const sql = 'CREATE DATABASE quotes2';
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw { ...err }
+        }
+        res.status(200).send('Database SUCCESSFULLY created!', result)
+    })
+})
+
+app.get('/drop-database', (req, res) => {
+    const sql = 'DROP DATABASE quotes2';
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            // res.status(500).send({...err})
+            throw { ...err }
+        }
+        res.status(200).send('Database SUCCESSFULLY dropped.')
+    })
+})
+
+app.get('/create-table', (req, res) => {
+    const sql =
+        `CREATE TABLE quotes(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            author VARCHAR(50) NOT NULL,
+            text VARCHAR(255) NOT NULL,
+            category ENUM('Sport', 'Fantasy', 'Comedy', 'History'));`
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw {...err}
+        }
+        res.status(200).send('Database table SUCCESSFULLY created!')
+    })
+})
