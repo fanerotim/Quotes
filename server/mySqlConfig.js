2242471751205761978e4e99dd62c2afd1c0e5d4
Petar Cholakov <petar.cholakov30@gmail.com>
2025-05-30 18:18:33 +0100

    create mysql conf file

---
+++ server/mySqlConfig.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const mysqlConfig = () => {
+    return mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    })
+}
+
+module.exports = mysqlConfig;
+
+
+
