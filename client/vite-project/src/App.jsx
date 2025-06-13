c8a1e7002b558abff0ddc6ee9ed2784a64e55414
Petar Cholakov <petar.cholakov30@gmail.com>
2025-06-13 15:15:29 +0100

    play around with closeup function / show loading... msg on component unmount

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -3,12 +3,22 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [quotes, setQuotes] = useState([]);
+  const [isUnmounting, setIsUnmounting] = useState(false)  
 
   useEffect(() => {
     fetch(import.meta.env.VITE_QUOTES_URL)
       .then(response => response.json())
-      .then(data => setQuotes(data));
+      .then(data => {
+        setQuotes(data)
+        setIsUnmounting(false)
+      });
+
+    return (() => setIsUnmounting(true))
   }, [])
+
+  if (isUnmounting) {
+    return (<h1>Loading...</h1>)
+  }
 
   return (
     <>
