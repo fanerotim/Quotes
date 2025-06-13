6098a05ea85dfb0580c79feaf355d37ce16cf14f
Petar Cholakov <petar.cholakov30@gmail.com>
2025-06-13 07:38:28 +0100

    add env variables - test with a request to get all quotes from db

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,12 +1,36 @@
 import './App.scss'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_QUOTES_URL)
+      .then(response => response.json())
+      .then(data => setQuotes(data));
+  }, [])
 
   return (
     <>
-      
+      <section>
+        {quotes.map(q => (
+          <div 
+            key={q.id} 
+            className='item'>
+            <h3>{q.author}</h3>
+            <p>{q.text}</p>
+          </div>
+          
+        ))}
+      </section>
     </>
   )
 }
+
+
+
+
+
+
 
 export default App
