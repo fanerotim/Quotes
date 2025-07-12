e9e9e0ded1519282e367f9e7b1d804b1ae01a5d0
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-12 19:30:50 +0100

    create quotes component

---
+++ client/vite-project/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+
+    return ( 
+        <>
+            <h1>Welcome to My Quotes App</h1>
+        </>
+    )
+}
+
+export default Home;
---
+++ client/vite-project/src/components/quotes/Quotes.jsx
@@ -0,0 +1,9 @@
+const Quotes = () => {
+    return (
+        <>
+            <h1>Welcome to my Quotes!</h1>
+        </>
+    )
+}
+
+export default Quotes;
