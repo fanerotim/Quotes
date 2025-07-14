1d26e6993b93aeacdb09d333e22a0d768d0c35e5
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-14 21:05:17 +0100

    add functionality for adding a quote and a demo form

--- client/vite-project/src/components/add-quote/AddQuote.jsx
+++ client/vite-project/src/components/add-quote/AddQuote.jsx
@@ -1,9 +1,57 @@
+import './AddQuote.scss'
+import useForm from '../hooks/useForm';
+
+const initialValues = {
+    author: '',
+    text: '',
+    category: ''
+}
+
 const AddQuote = () => {
 
+    const { values, handleChange } = useForm(initialValues);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('user data', values)
+    }
 
     return (
         <>
-            <h1>Add quote</h1>
+            <form
+                onSubmit={(e) => handleSubmit(e)}>
+                <section>
+                    <label>Author</label>
+                    <input
+                        onChange={handleChange} 
+                        value={values.author}
+                        name='author'
+                        type="text"/>
+                </section>
+
+                <section>
+                    <label>Text</label>
+                    <input 
+                        onChange={handleChange}
+                        value={values.text}
+                        name='text'
+                        type="text"/>
+                </section>
+
+                <section>
+                    <label>Category</label>
+                    <select 
+                        onChange={handleChange}
+                        name="category"
+                        value={values.category}>
+                        <option value="sport">Sport</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="history">History</option>
+                    </select>
+
+                </section>
+                <button>Submit</button>
+            </form>
         </>
     )
 }
