05280772559d8a7a15e73c1d233f2f9c0b615202
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-15 21:15:09 +0100

    add functionality for post requests to requester

--- client/vite-project/src/requester/http.jsx
+++ client/vite-project/src/requester/http.jsx
@@ -1,13 +1,25 @@
-const http = async (method, url, options) => {
- 
-    const response = await fetch(url)
+const http = async (method, url, values) => {
+    
+    const options = {};
+
+    if (method !== 'GET') {
+        options.headers = {
+            'Content-Type': 'application/json'
+        }
+        options.method = method;
+        options.body = JSON.stringify(values);
+    }
+
+    const response = await fetch(url, options)
     const result = await response.json()
 
     return result;
 }
 
 const get = http.bind('', 'GET');
+const post = http.bind('', 'POST');
 
 export default {
     get,
+    post
 }
