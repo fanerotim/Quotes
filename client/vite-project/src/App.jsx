e7d5265dcb5144ecd175511bbe7dc1b2348d0d3c
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-16 19:16:30 +0100

    add a route to edit quote page

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.scss'
-import AddQuote from './components/add-quote/addQuote';
 import Home from './components/home/Home';
 import Quotes from './components/quotes/Quotes';
+import AddQuote from './components/add-quote/addQuote';
+import EditQuote from './components/edit-quote/EditQuote';
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/quotes' element={<Quotes />}></Route>
         <Route path='/quotes/add-quote' element={<AddQuote/>}></Route>
+        <Route path='/quotes/edit-quote' element={<EditQuote/>}></Route>
       </Routes>
     </>
   )
