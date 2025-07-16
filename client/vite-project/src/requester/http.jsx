bee2ec420a3d27e5e394324cce62ca3af7a6210a
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-16 19:16:17 +0100

    add put method to requester

--- client/vite-project/src/requester/http.jsx
+++ client/vite-project/src/requester/http.jsx
@@ -1,5 +1,5 @@
 const http = async (method, url, values) => {
-    
+
     const options = {};
 
     if (method !== 'GET') {
@@ -11,6 +11,15 @@ const http = async (method, url, values) => {
     }
 
     const response = await fetch(url, options)
+
+    // TODO: CREATE AN EXAMPLE FOR THE FOLLOWING
+    // The fetch() function will reject the promise on some errors, 
+    // but not if the server responds with an error status like 404: so we also check the response status and throw if it is not OK.
+
+    if (!response.ok) {
+        throw new Error('Error in making the request', response.status)
+    }
+
     const result = await response.json()
 
     return result;
@@ -18,8 +27,10 @@ const http = async (method, url, values) => {
 
 const get = http.bind('', 'GET');
 const post = http.bind('', 'POST');
+const put = http.bind('', 'PUT');
 
 export default {
     get,
-    post
+    post,
+    put
 }
