35953f2b9cd5ba5eb94318417c96f10fb4fe9091
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-25 20:14:41 +0100

    add userController and a router for user registration

--- client/vite-project/src/hooks/useRegister.js
+++ client/vite-project/src/hooks/useRegister.js
@@ -1,13 +1,22 @@
+import http from "../requester/http"
+
 const useRegister = () => {
 
     const register = async ({ email, password, rePassword }) => {
 
+        // TODO: ADD ERROR HANDLING
         if (email === '' || password === '' || rePassword === '') {
             console.log('CANNOT REGISTER WITH EMPTY FILEDS')
         }
 
         if (password !== rePassword) {
             console.log('Password mismatch')
+        }
+
+        try {
+            const newUser = await http.post('http://localhost:3000/user/register', {email, password, rePassword})
+        } catch(err) {
+            console.log(err);
         }
     }
 
--- server/router/routes.js
+++ server/router/routes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //quote controller will hold all routes related to quotes
-const quoteController = require('../controllers/quoteController')
+const quoteController = require('../controllers/quoteController');
+const userController = require('../controllers/userController');
+
 router.use('/quotes', quoteController)
+router.use('/user', userController)
 
 module.exports = router;
