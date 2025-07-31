face99778274f21d217f24be4aa373372d733ec9
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-31 19:54:23 +0100

    complete query for user registration (still demo - no error handling yet)

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,7 +1,21 @@
-const register = async (email, password) => {
-    console.log('following are my email and password (comma separated)', email, password)
-}
+const mysqlConfig = require('../mySqlConfig');
+const db = mysqlConfig();
 
+const register = async (email, password) => {
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO users
+                    (email, password)
+                    VALUES(?, ?)`;
+
+        db.query(sql, [email, password], (err, result) => {
+            if (err) {
+                return reject(err)
+            }
+            return resolve(result);
+        })
+    })
+
+}
 
 module.exports = {
     register
