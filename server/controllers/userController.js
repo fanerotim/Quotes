56ce4f1af705ec131057d31eda025035c44e093a
Petar Cholakov <petar.cholakov30@gmail.com>
2025-07-31 19:31:00 +0100

    create a userService that will handle user events (register, login, etc)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const userService = require('../services/userService')
 
-router.post('/register', (req, res) => [
-    console.log(req.body)
-])
+router.post('/register', async (req, res) => {
+    const { email, password, rePassword } = req.body;
+
+    try {
+        const registeredUser = await userService.register(email, password);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ ...err })
+    }
+})
 
 module.exports = router;
---
+++ server/services/userService.js
@@ -0,0 +1,8 @@
+const register = async (email, password) => {
+    console.log('following are my email and password (comma separated)', email, password)
+}
+
+
+module.exports = {
+    register
+}
