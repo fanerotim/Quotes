6567bbf47226416d0f0e0e7ea52cbf2a5fba5b2e
Petar Cholakov <petar.cholakov30@gmail.com>
2025-08-02 20:27:11 +0100

    hash user password before adding user and their pass to the db

--- server/services/userService.js
+++ server/services/userService.js
@@ -18,20 +18,23 @@ const hasUser = async (email) => {
 
 const register = async (email, password) => {
 
-    //First check if user with this email is already registered
+    // first check if user with this email is already registered
     const userExists = await hasUser(email);
 
     // throw error if user already registered (we get an array, so check item at index 0 to verify if user exists)
     if (userExists[0]) {
         throw new Error('User is already registered. Please try again.')
     }
+
+    // if user does not exist hash their password
+    const hashedPassword = await bcrypt.hash(password, 10);
     
     return new Promise((resolve, reject) => {
         const sql = `INSERT INTO users
                     (email, password)
                     VALUES(?, ?)`;
 
-        db.query(sql, [email, password], (err, result) => {
+        db.query(sql, [email, hashedPassword], (err, result) => {
             if (err) {
                 return reject(err)
             }
