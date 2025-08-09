9a35daa6743665aacf09c986efe6798649edb3d4
Petar Cholakov <petar.cholakov30@gmail.com>
2025-08-09 16:41:30 +0300

    add user token to localStorage

--- client/vite-project/src/hooks/useLogin.js
+++ client/vite-project/src/hooks/useLogin.js
@@ -5,8 +5,12 @@ const useLogin = () => {
     const login = async (values) => {
         
         try {
-            const token = await http.post(`${import.meta.env.VITE_USER_URL}/login`, values)
-            return token
+            const token = await http.post(`${import.meta.env.VITE_USER_URL}/login`, values);
+
+            // add token to localStorage
+            localStorage.setItem('accessToken', JSON.stringify(token));
+
+            return token;
         } catch(err) {
             console.log(err);
         }
