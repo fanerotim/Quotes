12ace1e9207dc4395b37d723bfeace1237f0b694
Petar Cholakov <petar.cholakov30@gmail.com>
2025-08-15 21:47:46 +0300

    update useLogin to also use dispatch auth reducer

--- client/vite-project/src/hooks/useLogin.js
+++ client/vite-project/src/hooks/useLogin.js
@@ -1,17 +1,23 @@
 import http from '../requester/http';
+import { useAuth } from '../contexts/authContext';
 
 const useLogin = () => {
 
+    const { dispatch } = useAuth();
+
     const login = async (values) => {
-        
+
         try {
             const token = await http.post(`${import.meta.env.VITE_USER_URL}/login`, values);
 
             // add token to localStorage / TODO: make this abstract, so it does not have to exist in useRegister too;
-            localStorage.setItem('accessToken', JSON.stringify(token));
+            dispatch({
+                type: 'LOGIN',
+                accessToken: token.auth,
+            })
 
-            return token;
-        } catch(err) {
+            // return token;
+        } catch (err) {
             console.log(err);
         }
     }
