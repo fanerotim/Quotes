fe315d95ab8d53194cd54f12c37dcfc5de72968c
Petar Cholakov <petar.cholakov30@gmail.com>
2025-08-15 22:05:24 +0300

    add conditional rendering for certain routes for dev purposes

--- client/vite-project/src/components/navigation/Navigation.jsx
+++ client/vite-project/src/components/navigation/Navigation.jsx
@@ -1,17 +1,20 @@
 import './Navigation.scss'
 import { Link } from "react-router-dom";
 import Logout from '../logout/logout';
+import { useAuth } from '../../contexts/authContext';
 
 const Navigation = () => {
+
+    const { auth } = useAuth();
 
     return (
         <nav>
             <Link to='/'>Home</Link>
             <Link to='/quotes'>Quotes</Link>
-            <Link to='/quotes/add-quote'>Add quote</Link>
+            {auth ? <Link to='/quotes/add-quote'>Add quote</Link> : ''}
             <Link to='/users/register'>Register</Link>
             <Link to='/users/login'>Login</Link>
-            <Logout/>
+            {auth ? <Logout /> : ''}
         </nav>
     )
 }
