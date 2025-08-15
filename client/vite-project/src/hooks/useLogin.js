0814f3539b827f77266e41447be4a884ea904b9e
Petar Cholakov <petar.cholakov30@gmail.com>
2025-08-15 22:04:15 +0300

    fix accessToken formatting as it was causing REST API errors

--- client/vite-project/src/hooks/useLogin.js
+++ client/vite-project/src/hooks/useLogin.js
@@ -3,21 +3,24 @@ import { useAuth } from '../contexts/authContext';
 
 const useLogin = () => {
 
-    const { dispatch } = useAuth();
+    const {dispatch} = useAuth();
 
     const login = async (values) => {
-
+        
         try {
             const token = await http.post(`${import.meta.env.VITE_USER_URL}/login`, values);
 
             // add token to localStorage / TODO: make this abstract, so it does not have to exist in useRegister too;
+            // localStorage.setItem('accessToken', JSON.stringify(token));
+            // server expects token to be added in this format to the localStorage: {"auth":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImlAYWJ2LmJnIiwiaWF0IjoxNzU1Mjg0MzAzLCJleHAiOjE3NTUyOTg3MDN9.1Axi99lTVM19_H_E8eJSy-uyqifBE3HWxR_JIBxh0WU"}    
+        
             dispatch({
                 type: 'LOGIN',
-                accessToken: token.auth,
+                accessToken: token,
             })
 
             // return token;
-        } catch (err) {
+        } catch(err) {
             console.log(err);
         }
     }
