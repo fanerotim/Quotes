1217af58c7d2916b26665591c6ce691fe08d7cab
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-17 22:15:55 +0300

    improve auth rendering logic on nav links

--- client/vite-project/src/components/navigation/Navigation.jsx
+++ client/vite-project/src/components/navigation/Navigation.jsx
@@ -5,16 +5,18 @@ import { useAuthContext } from '../../hooks/useAuthContext';
 
 const Navigation = () => {
 
+    // TODO: fix this bug - on refresh auth.email is no longer working
     const { auth } = useAuthContext();
 
     return (
         <nav>
+            {auth && (<span className='greeting'>Hello, {auth.email}</span>)}
             <Link to='/'>Home</Link>
             <Link to='/quotes'>Quotes</Link>
-            {auth ? <Link to='/quotes/add-quote'>Add quote</Link> : ''}
-            {!auth ? <Link to='/users/register'>Register</Link> : ''}
-            {!auth ? <Link to='/users/login'>Login</Link> : ''}
-            {auth ? <Logout /> : ''}
+            {auth && (<Link to='/quotes/add-quote'>Add quote</Link>)}
+            {!auth && (<Link to='/users/register'>Register</Link>)}
+            {!auth && (<Link to='/users/login'>Login</Link>)}
+            {auth && (<Logout />)}
         </nav>
     )
 }
