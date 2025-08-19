dc0823eb029377a4c200bcecb97949de2111110e
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-19 08:09:16 +0300

    if token was tampered with - logout user and redirect them to login
    still need to show user why this happened / not done yet

--- client/vite-project/src/components/edit-quote/EditQuote.jsx
+++ client/vite-project/src/components/edit-quote/EditQuote.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import useForm from '../../hooks/useForm';
 import './EditQuote.scss'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import useEditQuote from '../../hooks/useEditQuote';
 import useGetQuote from '../../hooks/useGetQuote';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const EditQuote = () => {
 
@@ -17,19 +18,31 @@ const EditQuote = () => {
     const { values, handleChange } = useForm(quote);
     const { edit } = useEditQuote();
     const { getQuote } = useGetQuote();
-    
+    const { dispatch } = useAuthContext();
+    const navigate = useNavigate();
 
     useEffect(() => {
         // this piece of code makes sure edit form is prepopulated with accurate data on initial load
-        const newQuote = 
-        getQuote(quoteId)
-        .then(result => setQuote(result))
-        .catch(err => console.log(err))
+        const newQuote =
+            getQuote(quoteId)
+                .then(result => setQuote(result))
+                .catch(err => console.log(err))
     }, []);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const updatedQuote = edit(values, quoteId);
+
+        try {
+            const updatedQuote = await edit(values, quoteId);
+        } catch (err) {
+            // if token has been modified (in local storage) and didn't verify
+            // then logout and navigate to login
+            // TODO: show an error to the user
+            dispatch({
+                type: 'LOGOUT'
+            });
+            navigate('/users/login');
+        }
     }
 
     return (
