a70bfab8f6059611e5b8527a01481c6b8c780897
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-24 08:51:51 +0300

    hide edit / delete buttons if user is not logged in
    demo implementation as i will hide buttons if user is not owner of quote, even if logged in

--- client/vite-project/src/components/quote-details/QuoteDetails.jsx
+++ client/vite-project/src/components/quote-details/QuoteDetails.jsx
@@ -4,27 +4,31 @@ import useGetQuote from "../../hooks/useGetQuote";
 import { useEffect, useState } from "react";
 import EditButton from "../buttons/edit-btn/EditButton";
 import DeleteButton from "../buttons/delete-btn/DeleteButton";
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const QuoteDetails = () => {
 
     const { quoteId } = useParams();
     const { getQuote } = useGetQuote();
     const [quote, setQuote] = useState(null)
-    
+
+    // for dev purposes, for now I am only checking if the user is logged in, but eventually i would like to check if user is owner of the quote. other users cannot edit / delete
+    const { auth } = useAuthContext();
+
     useEffect(() => {
         getQuote(quoteId)
             .then(data => setQuote(data))
             .catch(err => console.error(err));
     }, [])
-        
+
     return (
         <section className="quote-details__container">
             <h1>Welcome to quote details page</h1>
             <h2>{quote?.text}</h2>
             <h3>{quote?.author}</h3>
-            <DeleteButton quoteId={quote?.id}/>
-            <br/>
-            <EditButton id={quote?.id}/>
+            {auth && <DeleteButton quoteId={quote?.id} />}
+            <br />
+            {auth && <EditButton id={quote?.id} />}
         </section>
     )
 }
