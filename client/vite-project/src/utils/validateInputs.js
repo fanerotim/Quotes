7a1241c3412246ed42722904fc6f3c54edb98b75
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-24 19:16:26 +0300

    clarify comment on helper validateInput function

--- client/vite-project/src/utils/validateInputs.js
+++ client/vite-project/src/utils/validateInputs.js
@@ -1,7 +1,7 @@
 const validateInputs = (inputAsObj) => {
 
-    // filter the input object to make sure it does not contain value
-    // value is a number, so if kept then .trim() method breaks as it's a string method
+    // filter the input object to make sure it does not contain id
+    // id is of type number, so if kept then .trim() method returns error, as it's a string method
     const inputAsArr = Object.entries(inputAsObj)
         .filter(([key, value]) => key !== 'id')
         .map(([key, value]) => value);
