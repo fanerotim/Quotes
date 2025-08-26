63bdc73dbd50e0f2b30ce9f75ad3e8f4589c621d
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-26 23:40:41 +0300

    wrap app in context to be able to use context in app comp

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import AuthContextProvider from './contexts/AuthContextProvider.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <AuthContextProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </AuthContextProvider>
 
 )
