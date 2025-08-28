4b0658acd4e5950c0bf03a503a775834cc169439
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-28 19:06:43 +0300

    update useDelete custom hook to change sessionExpired value in case token is invalid / expired

--- client/vite-project/src/hooks/useDelete.js
+++ client/vite-project/src/hooks/useDelete.js
@@ -1,23 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import http from "../requester/http";
+import useLogoutOn401Error from "./useLogoutOn401Error";
 
 const useDelete = () => {
 
     const navigate = useNavigate();
+    const { useLogoutOn401 } = useLogoutOn401Error();
 
     const deleteOne = async (url, values) => {
 
         try {
             await http.deleteOne(`${import.meta.env.VITE_BASE_URL}/${url}`, values)
             navigate('/quotes')
-            // There is no item to return as I return success msg;
+            // There is no returned item, as I simply return success msg;
             // TODO: think if you want to return a success msg instead of redirecting
-            // return deletedItem;
-        } catch(err) {
-            return err;
+        } catch (err) {
+            useLogoutOn401(err);
+            throw err;
         }
     }
-    
+
     return {
         deleteOne
     }
