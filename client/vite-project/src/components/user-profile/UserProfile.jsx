c742a1085dbb1f1f9f856d52787b2dd9ab49c959
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-28 22:34:40 +0300

    create user profile component

---
+++ client/vite-project/src/components/user-profile/UserProfile.jsx
@@ -0,0 +1,11 @@
+import './UserProfile.scss'
+
+const UserProfile = () => {
+    return (
+        <>
+            <h1>Welcome to User Profile page</h1>
+        </>
+    )
+}
+
+export default UserProfile;
