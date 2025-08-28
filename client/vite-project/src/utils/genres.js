7bd76ea7b362f42b02ed09520235d10ebf53d73f
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-28 19:25:49 +0300

    create a util object that holds book genres

---
+++ client/vite-project/src/utils/genres.js
@@ -0,0 +1,66 @@
+export const GENRES = [
+    {
+        id: "Fiction"
+    },
+        {
+        id: "Novel"
+    },
+        {
+        id: "Science-Fiction"
+    },
+        {
+        id: "Short-Story"
+    },
+        {
+        id: "Memoir"
+    },
+        {
+        id: "Biography"
+    },
+        {
+        id: "Thriller"
+    },
+        {
+        id: "History"
+    },
+        {
+        id: "Autobiography"
+    },
+        {
+        id: "Humor"
+    },
+        {
+        id: "Science"
+    },
+        {
+        id: "Fairy-Tale"
+    },
+        {
+        id: "Poetry"
+    },
+        {
+        id: "Drama"
+    },
+        {
+        id: "Satire"
+    },
+        {
+        id: "Psychology"
+    },
+        {
+        id: "Social-Science"
+    },
+        {
+        id: "Magical-Realism"
+    },
+        {
+        id: "Self-Help"
+    },
+        {
+        id: "Sports"
+    },
+        {
+        id: "Art"
+    },
+
+]
