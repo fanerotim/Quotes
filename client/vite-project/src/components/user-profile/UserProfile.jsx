1b5d56e2bac6f7cbb617e306afa509d5b44c37c9
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-29 22:15:11 +0300

    add functionality to UserProfile component to retrieve quotes added by a user
    initial iteration, just making it work. optimization to follow

--- client/vite-project/src/components/user-profile/UserProfile.jsx
+++ client/vite-project/src/components/user-profile/UserProfile.jsx
@@ -1,9 +1,28 @@
 import './UserProfile.scss'
+import useGetUserQuotes from '../../hooks/useGetUserQuotes';
+import { useEffect, useState } from 'react';
 
 const UserProfile = () => {
+
+    const { getUserQuotes } = useGetUserQuotes();
+    const [userQuotes, setUserQuotes] = useState([]);
+    
+    useEffect(() => {
+        getUserQuotes()
+            .then(quotes => setUserQuotes(quotes))
+            .catch(err => console.error(quotes));
+    }, [])
+
     return (
         <>
-            <h1>Welcome to User Profile page</h1>
+            <h1>Please find the quotes you have added below:</h1>
+            {userQuotes.map((quote) => (
+                <div>
+                    <p>{quote.author}</p>
+                    <h3>{quote.text}</h3>
+                    <h5>{quote.category}</h5>
+                </div>
+            ))}
         </>
     )
 }
