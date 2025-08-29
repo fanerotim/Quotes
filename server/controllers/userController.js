74e192b8741a889eddb93ea72230d7ba42beae6c
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-29 19:13:36 +0300

    craete a util file, which will store db creation during dev, as db schemas are changing as i develop the app

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,9 +5,11 @@ router.post('/register', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // TODO: I do not really want to return the token, as we are not logging the user automatically
+        // TODO: Fix this
+        // I do not really want to return the token for now, as currently we are not logging the user automatically
+        // so now, I have the token, but am not doing anything with it;
         const token = await userService.register(email, password);
-        res.status(200).json('User successfully registered');
+        res.status(200).json({message: 'User successfully registered'});
     } catch (err) {
         const status = err.statusCode || 500;
         res.status(status).json({ message: err.message })
@@ -18,8 +20,12 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const token = await userService.login(email, password);
-        res.status(200).json({ "auth": token, email })
+        const authData = await userService.login(email, password);
+        res.status(200).json({ 
+            auth: authData.token, 
+            email: authData.email, 
+            id: authData.id 
+        })
     } catch (err) {
         const status = err.statusCode || 500;
         res.status(status).json({ message: err.message })
@@ -35,7 +41,7 @@ router.post('/logout', async (req, res) => {
         return res.status(200).json({ message: 'Successfully logged out' })
     } catch (err) {
         const status = err.statusCode || 500;
-        return res.status(status).json({message: err.message})
+        return res.status(status).json({ message: err.message })
     }
 })
 
---
+++ server/utils/DEV-db-table-configuration/quotes-table.js
@@ -0,0 +1,29 @@
+// saving this CREATE TABLE statement for dev purposes, to avoid having to write it again if needed during dev as ENUM is quite long:
+// CREATE TABLE quotes (
+// 	id INT PRIMARY KEY AUTO_INCREMENT,
+//     author varchar(300),
+//     text longtext,
+//     ownerId INT,
+//     category ENUM(
+//     "Fiction", 
+//     "Novel", 
+//     "Science-Fiction", 
+//     "Short-Story",
+//     "Memoir",
+//     "Biography",
+//     "Thriller",
+//     "History",
+//     "Autobiography",
+//     "Humor",
+//     "Science",
+//     "Fairy-Tale",
+//     "Poetry",
+//     "Drama",
+//     "Satire",
+//     "Psychology",
+//     "Social-Science",
+//     "Magical-Realism",
+//     "Self-Help",
+//     "Sports",
+//     "Art")
+// )
