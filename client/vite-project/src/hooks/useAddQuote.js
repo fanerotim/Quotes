600adca2612b2400ed0668b42538fffa40ee4db8
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-29 19:12:27 +0300

    include userId among values as ownerId and send it to REST API

--- client/vite-project/src/hooks/useAddQuote.js
+++ client/vite-project/src/hooks/useAddQuote.js
@@ -2,21 +2,27 @@ import { useState } from "react";
 import http from "../requester/http";
 import validateInputs from "../utils/validateInputs";
 import useLogoutOn401Error from "./useLogoutOn401Error";
+import useGetAccessToken from "./useGetAccessToken";
 
 const useAddQuote = () => {
     const [error, setError] = useState(null);
     const [isLoading, setLoading] = useState(false);
-    const {useLogoutOn401} = useLogoutOn401Error();
+    const { useLogoutOn401 } = useLogoutOn401Error();
+    // TODO: getToken function retrieves not only the token, but also user email and id
+    // Fix this: improve naming, maybe getUserAuthData, or something like that
+    const getToken = useGetAccessToken();
 
     const addQuote = async (values) => {
         setError(null);
-
+        
         try {
             validateInputs(values);
+            // TODO: check above comment: getToken() should be renamed
+            const userId = getToken().id;
             setLoading(true);
-            const newQuote = await http.post(`${import.meta.env.VITE_BASE_URL}/add-quote`, values);
+            const newQuote = await http.post(`${import.meta.env.VITE_BASE_URL}/add-quote`, {...values, ownerId: userId});
             return newQuote;
-        } catch (err) {   
+        } catch (err) {
             // check if error is 401 to logout the user
             useLogoutOn401(err);
             // standardaze error handling // with this set up err.message the validator errors do not show, if i set it to err, then the front-end breaks as error comes as obj
