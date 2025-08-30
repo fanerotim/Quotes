a0d591cf77a1f554938a102d64920ef0d430f8e5
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-30 18:22:06 +0300

    add some basic styles for user profile component to not be utterly ugly even during dev time

--- server/utils/validateInputs.js
+++ server/utils/validateInputs.js
@@ -1,6 +1,6 @@
 // util that does basic validation; accepts string[], loops through it and if empty value is found throw an error
 exports.validateInputs = (inputsArr) => {
-
+    console.log(typeof inputsArr[0]);
     inputsArr.forEach(input => {
         if (!input || !input.trim()) {      
             const error = new Error('All fileds must be filled.');
