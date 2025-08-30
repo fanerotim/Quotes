ca610eac35715047549316537237f3d5481c38dc
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-30 19:38:18 +0300

    rename and convert isAuth middleware to a route guard for clarity

--- server/middlewares/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-// this middleware checks if user is logged in and if not returns a forbidden error
-exports.isAuth = (req, res, next) => {
-    const userData = req.user;
-    
-    if (!userData) {
-        return res.status(403).json({error: 'Access Forbidden'})
-    }
-    next();
-}
