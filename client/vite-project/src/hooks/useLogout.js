6e0f9c606bf73874f7a8f336522ae2931aee0221
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-30 19:37:06 +0300

    add a check if on logout the server returns 401 error and logs out the user in case it does, as i found a bug with requests made by postman

--- client/vite-project/src/hooks/useLogout.js
+++ client/vite-project/src/hooks/useLogout.js
@@ -1,15 +1,18 @@
 import http from "../requester/http";
 import { useAuthContext } from '../hooks/useAuthContext'
 import useGetAccessToken from "./useGetAccessToken";
+import useLogoutOn401Error from './useLogoutOn401Error';
 
 const useLogout = () => {
 
     const { dispatch } = useAuthContext();
     const getToken = useGetAccessToken();
     const accessToken = getToken();
+    // TODO: FIX THIS, I NEED TO ABSTRACT THIS LOGIC AS I AM FINDING MYSELF HAVING TO ADD THE BELOW FUNCTION IN EVERY COMPONENT THAT MAKES AUTHORIZED REQUESTS
+    const { useLogoutOn401 } = useLogoutOn401Error();
 
     const logout = async () => {
-        
+
         try {
             const result = await http.post(`${import.meta.env.VITE_USER_URL}/logout`, accessToken)
 
@@ -17,6 +20,7 @@ const useLogout = () => {
                 type: 'LOGOUT'
             })
         } catch (err) {
+            useLogoutOn401(err);
             console.error(err);
         }
     }
