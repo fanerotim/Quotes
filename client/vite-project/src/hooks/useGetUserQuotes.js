c2ef46ee7e260aff06cbf73a15e846bd14ee9096
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-31 17:31:20 +0300

    change behavior of getUserQuotes - no need to send userId,  this will be handled by the server completely

--- client/vite-project/src/hooks/useGetUserQuotes.js
+++ client/vite-project/src/hooks/useGetUserQuotes.js
@@ -1,18 +1,17 @@
 import http from "../requester/http";
-import useGetAccessToken from "./useGetAccessToken";
+import useLogoutOn401Error from '../hooks/useLogoutOn401Error';
 
 const useGetUserQuotes = () => {
 
-    const getToken = useGetAccessToken();
-    // TODO: fix this; for now using optional chaining; improve it;
-    const userId = getToken()?.id;
-   
+    const { logoutOn401 } = useLogoutOn401Error();
+
     const getUserQuotes = async () => {
 
         try {
-            const userQuotes = await http.post(`${import.meta.env.VITE_BASE_URL}/user-quotes`, { userId });
+            const userQuotes = await http.post(`${import.meta.env.VITE_BASE_URL}/user-quotes`);
             return userQuotes;
         } catch (err) {
+            logoutOn401(err);
             console.error(err);
         }
     }
