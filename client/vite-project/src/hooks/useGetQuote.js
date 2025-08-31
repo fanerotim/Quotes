1ce510531f082d28e9dcef3ac711b3cdab20d735
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-31 17:30:16 +0300

    wrap api call in try catch

--- client/vite-project/src/hooks/useGetQuote.js
+++ client/vite-project/src/hooks/useGetQuote.js
@@ -3,8 +3,13 @@ import http from "../requester/http";
 const useGetQuote = () => {
 
     const getQuote = async (quoteId) => {
-        const newQuote = await http.get(`${import.meta.env.VITE_BASE_URL}/${quoteId}`);
-        return newQuote[0];
+        try {
+            const newQuote = await http.get(`${import.meta.env.VITE_BASE_URL}/${quoteId}`);
+            return newQuote[0];
+        } catch (err) {
+            return err;
+        }
+
     }
 
     return {
