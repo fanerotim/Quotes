0d890668faf824bf5c0e049c97a5a09cef68984a
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-08-31 17:29:55 +0300

    add conditional chaining to fix a bug which breaks the page if user quotes is empty

--- client/vite-project/src/components/user-profile/UserProfile.jsx
+++ client/vite-project/src/components/user-profile/UserProfile.jsx
@@ -16,7 +16,8 @@ const UserProfile = () => {
     return (
         <section className='page__wrapper'>
             <h1>Please find the quotes you have added below:</h1>
-            {userQuotes.map((quote) => (
+            {/* TODO: fix this. It would be better if we can not use conditional chaining */}
+            {userQuotes?.map((quote) => (
                 <div className='quote__container' key={quote.id}>
                     <h1>{quote.author}</h1>
                     <p className='quote__container__quote__text'>{quote.text}</p>
