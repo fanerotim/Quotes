03d5a67cb572b19f2a5e12b4d29c70c5090e980b
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-01 23:00:27 +0300

    create a util function that generates random passwords upon user request for password reset

---
+++ server/utils/generateRandomPassword.js
@@ -0,0 +1,18 @@
+// use this library for now to finish the feature, but build my own password generator once i have more time
+const generator = require('generate-password');
+
+const generateRandomPassword = () => {
+    const password = generator.generate({
+        length: 30,
+        numbers: true,
+        symbols: true,
+        uppercase: true,
+        lowercase: true
+    })
+
+    return password;
+}
+
+module.exports = {
+    generateRandomPassword
+}
