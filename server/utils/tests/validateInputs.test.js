35f45a74754f74610159ce0604339b5c46c10569
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-04 21:42:33 +0300

    test validateInputs fn and correct typing mistake in the error in returns

---
+++ server/utils/tests/validateInputs.test.js
@@ -0,0 +1,17 @@
+const { validateInputs } = require('../validateInputs');
+
+test('should throw', () => {
+    expect(() => validateInputs(2, 3).toThrow());
+})
+
+test('should throw new Error', () => {
+    expect(() => validateInputs(1, 2).toThrowError());
+})
+
+test('should throw error', () => {
+    expect(() => validateInputs([' ', ''])).toThrow(Error)
+})
+
+test('should return true', () => {
+    expect(() => validateInputs(["pom@abv.bg", "123"])).toBeTruthy();
+})
--- server/utils/validateInputs.js
+++ server/utils/validateInputs.js
@@ -1,12 +1,15 @@
 // util that does basic validation; accepts string[], loops through it and if empty value is found throw an error
 exports.validateInputs = (inputsArr) => {
-    
+
     inputsArr.forEach(input => {
         if (!input || !input.trim()) {
-            const error = new Error('All fileds must be filled.');
+            const error = new Error('All fields must be filled.');
             error.statusCode = 400;
             throw error;
         }
-        return true;
+        // By mistake have kept the return value inside the forEach call and this function was returning undefined
+        // Moving return outside of this scope - will remove comments and commented return later as I want to test further
+        // return true;
     });
+    return true;
 }
