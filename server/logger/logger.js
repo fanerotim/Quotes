146f66b86668d0845510bd9813e5594d2aeee940
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-04 16:26:55 +0300

    add initial implementation of logger functionality

---
+++ server/logger/logger.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs/promises');
+const path = require('path');
+
+const LOG_PATHS = {
+    ACCESS_LOG: path.join(__dirname, '../logs/access.log'),
+    CRON_LOG: path.join(__dirname, '../logs/cron.log')
+}
+
+const logger = async (type, content) => {
+
+    const data = JSON.stringify(content);
+    const PATH = LOG_PATHS[type]
+
+    try {
+        await fs.appendFile(PATH, `\n${data}`, 'utf8');
+        console.log('LOGIN SUCCESSFULLY LOGGED')
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = {
+    logger
+}
