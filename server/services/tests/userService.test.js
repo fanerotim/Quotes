66ea79bf2599beaed6b89dfe2148c151a178b98f
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-06 22:53:51 +0300

    finally found the reason for my issue - all related to stupid mysql2 package

--- server/services/tests/userService.test.js
+++ server/services/tests/userService.test.js
@@ -1,12 +1,26 @@
 const userService = require('../userService');
 
 test('should return user data', () => {
-    userService.hasUser = jest.fn().mockReturnValue([{id: 1, email: 'i@abv.bg', password: 'asd'}]);
 
-    return userService.hasUser()
-        .then(result => {
-        expect(result).toEqual([{id: 1, email: 'i@abv.bg', password: 'asd'}])
+    userService.hasUser = jest.fn((email) => {
+        return Promise.resolve([{ id: 1, email: 'i@abv.bg', password: 'abv' }])
     })
+
+    return userService.hasUser('i@abv.bg')
+        .then(result => {
+            expect(result).toEqual([{ id: 1, email: 'i@abv.bg', password: 'abv' }])
+        })
+})
+
+test('should return empty array', () => {
+    userService.hasUser = jest.fn((email) => {
+        return Promise.resolve([]);
+    })
+
+    return userService.hasUser('p@abv.bg')
+        .then(result => {
+            expect(result).toEqual([]);
+        })
 })
 
 // test('resolves', () => {
