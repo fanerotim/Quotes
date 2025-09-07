1aa1cd504e4dfb3ffc1f2595e01e6b16a53268a8
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-07 17:55:20 +0300

    add tests for register method / testing to be continued

--- server/services/tests/userService.test.js
+++ server/services/tests/userService.test.js
@@ -1,6 +1,8 @@
-const mysqlConfig = require('../../mySqlConfig');
 const userService = require('../userService');
-const db = mysqlConfig();
+jest.mock('../userService'); // mock user service
+
+
+// hasUser method tests;
 
 test('should return user data if provided email exists in db', () => {
 
@@ -25,28 +27,55 @@ test('should return empty array if email is not found in db', () => {
         })
 })
 
-test('should reject with err', () => {
-    userService.hasUser = jest.fn(() => {
-        return Promise.reject(Error)
-    })
-
-    return userService.hasUser('test@abv.bg')
-        .catch(err => {
-            expect(err).toEqual(Error);
-        })
-})
-
 // adding this test, so I can try this way of testing as due to mysql2 issues, this type of test was failing for 2 days.
 // will standardize once i figure out how to fix the bug with mysql2
 test('promise should be rejected if db is down', () => {
-    
-    jest.mock('../userService');
 
-    const error = {message: 'db connection down'};
-    const response = {result: error};
+    const error = { message: 'db connection down' };
+    const response = { result: error };
     userService.hasUser.mockRejectedValue(response);
 
     return userService.hasUser().catch(err => {
         expect(err).toEqual(response);
     })
 })
+
+
+// register method tests
+
+test('should throw error with message `All fields must be filled.`', () => {
+
+    const error = { message: 'All fields must be filled', statusCode: 400 };
+    const userInput = { email: '', password: '123' }
+
+    userService.register = jest.fn((userInput) => {
+        return Promise.reject(error);
+    })
+
+    return userService.register(userInput)
+        .catch(err => {
+            expect(err).toEqual(error);
+        })
+})
+
+test('should throw `invalid credentials` error message', () => {
+
+    const error = new Error({ message: 'Invalid credentials!', statusCode: 409 });
+
+    userService.register.mockReturnValue(error);
+    expect(userService.register({ email: 'i@abv.bg', password: '123' })).toEqual(error);
+})
+
+test('should throw if user exits in db', () => {
+
+    const error = new Error('Invalid credentials!');
+
+    userService.register = jest.fn((email, password) => {
+        throw error;
+    })
+
+    expect(() => {
+        userService.register('i@abv.bg', '123')
+    }).toThrow(new Error('Invalid credentials!'));
+})
+
