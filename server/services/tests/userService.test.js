c8094cb86c39eb5740237242172f97537326ca99
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-08 23:40:46 +0300

    it's time for bed. still struggling to mock the db. updating file as after mySqlConfig was updated to singleton, now if I call the db I do not get the open async operation error

--- server/services/tests/userService.test.js
+++ server/services/tests/userService.test.js
@@ -1,21 +1,21 @@
 const userService = require('../userService');
-const mySqlConfig = require('../../mySqlConfig');
+let mysqlConfig = require('../../mySqlConfig');
+const db = mysqlConfig();
+
+// close db connection after each request fixes error with jest (open async operation)
+afterEach(() => {
+    db.end();
+})
 
 // custom error
 const missingInputError = new Error('hello, hello - user input is required :)');
 
-jest.mock('../../mySqlConfig');
-
 // hasUser() tests
-test.only('throw error if user already registered', () => {
-    const db = mySqlConfig();
-
-    db.query.mockResolvedValue([]);
+test('throw error if user already registered', () => {
 
     return userService.hasUser('b@abv.bg').then(data => {
         expect(data).toEqual([]);
     })
-
 });
 
 
