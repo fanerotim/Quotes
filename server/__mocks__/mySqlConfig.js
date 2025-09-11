f24f4392edbaae6ac147ad260b0a9da864ab6733
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-11 14:33:43 +0300

    add a comment of why I am not using the mocked module - will review it again when I can. moving on with the a working version of a successful mock of db.query

--- server/__mocks__/mySqlConfig.js
+++ server/__mocks__/mySqlConfig.js
@@ -2,12 +2,14 @@ const users = [
     { id: 1, email: 'i@abv.bg', password: '123' }
 ]
 
+// for now this is diabled as it took me quite a long time to figure out why the query: jest.fn() was not being overwritten by my tests in userService.test.js
+// I want to move on for now. Successfully mocked the function in userService.test.js. will come back to this when I can.
 const mysqlConfig = () => {
     const db = {
-        query: (sql, [email], callback) => {
-            callback(null, email);
-        } 
-        // query: jest.fn()
+        // query: (sql, [email], callback) => {
+        //     callback(null, email);
+        // } 
+        query: jest.fn()
     }
     return db;
 }
