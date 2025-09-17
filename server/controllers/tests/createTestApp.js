a488ecc1438c6fafdb760934ee927ef1bd51a7ea
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-17 16:31:15 +0300

    remove express config from the fn that creates a test app - i was getting a bug due to my jwt clearing cron and jest tests were timing out. detached the cron from the app and issue solved.

--- server/controllers/tests/createTestApp.js
+++ server/controllers/tests/createTestApp.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const expressConfig = require('../../expressConfig');
 const routes = require('../../router/routes');
 const app = express();
 
 const createTestApp = () => {
-    expressConfig(app);
     app.use(routes);
-
     return app;
 }
 
