098d88d66cec46d9745ffd3d882a992b6fa77a93
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-18 15:38:42 +0300

    add express.json() middleware to the test express instance

--- server/controllers/tests/createTestApp.js
+++ server/controllers/tests/createTestApp.js
@@ -3,6 +3,8 @@ const routes = require('../../router/routes');
 const app = express();
 
 const createTestApp = () => {
+    // it's important to have express.json() attached to the instance of expres before we attach the routes (otherwise it does not work)
+    app.use(express.json())
     app.use(routes);
     return app;
 }
