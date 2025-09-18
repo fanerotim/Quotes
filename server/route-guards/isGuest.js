b17e852c389f27aaa5e49f5919742c9e07e58aa6
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-18 14:09:43 +0300

    correct typo

--- server/route-guards/isGuest.js
+++ server/route-guards/isGuest.js
@@ -2,8 +2,8 @@ exports.isGuest = (req, res, next) => {
     const user = req.user;
 
     if (!user) {
-        return res.status(401).json({mesasge: 'You are not authorized to access this resource. Please log in!'})
+        return res.status(401).json({message: 'You are not authorized to access this resource. Please log in!'})
     }
 
-    next ();
+    next();
 }
