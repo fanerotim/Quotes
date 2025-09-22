b0c023d3e4c5103ecf3170a216ad065bc593bd55
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-22 15:47:17 +0300

    move click handler inside quote card component (removed from quotes) and consume useScroll hook to send quoteid to setter fn

--- client/vite-project/src/components/quote-card/QuoteCard.jsx
+++ client/vite-project/src/components/quote-card/QuoteCard.jsx
@@ -1,14 +1,27 @@
-import './QuoteCard.scss'
+import './QuoteCard.scss';
+import { useNavigate } from 'react-router-dom';
+import useScroll from '../../hooks/useScroll';
 
-const QuoteCard = ({ quote, clickHandler }) => {
+const QuoteCard = ({ quote, mapRefs }) => {
+
+    const navigate = useNavigate();
+    const { setQuoteId } = useScroll();
+
+    const clickHandler = (id) => {
+        setQuoteId(id);
+        navigate({
+            pathname: `/quotes/${id}}`
+        })
+    }
 
     return (
-        <article>
+        <article
+            ref={(node) => {
+                mapRefs.set(quote.id, node)
+            }}>
             <li>{quote.text}</li>
             <h2>{quote.author}</h2>
-            <button onClick={() => {
-                clickHandler(quote.id);
-            }}>
+            <button onClick={() => clickHandler(quote.id)}>
                 Show details
             </button>
         </article>
