93457b4bad0def972d362c46256584e9624c3b65
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-23 14:21:26 +0300

    after debugging for hours, finally i understood that i need to move the state in a context, so it can be consumed by my quotes component. it was being reset if i kept in useScroll custom hook

--- client/vite-project/src/hooks/useScroll.js
+++ client/vite-project/src/hooks/useScroll.js
@@ -1,13 +1,7 @@
 import { useRef } from "react";
 
-let quoteId = 0;
-
 const useScroll = () => {
     const quoteCardRefs = useRef(null);
-
-    const setQuoteId = (id) => {
-        quoteId = id;
-    }
 
     const createMap = () => {
 
@@ -17,8 +11,8 @@ const useScroll = () => {
         return quoteCardRefs.current;
     }
 
-    const scrollToItem = () => {
-        quoteCardRefs.current.get(quoteId)?.scrollIntoView({
+    const scrollToItem = (id) => {
+        quoteCardRefs.current.get(id)?.scrollIntoView({
             behavior: 'smooth',
             block: 'center'
         });
@@ -27,7 +21,6 @@ const useScroll = () => {
     return {
         createMap,
         scrollToItem,
-        setQuoteId
     }
 }
 
