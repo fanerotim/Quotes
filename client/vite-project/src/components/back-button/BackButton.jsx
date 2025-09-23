5101a4ae3d01bfd2ffb0f6554c0ec951548ffab8
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-23 20:56:52 +0300

    complete the implementation of my custom back button functionality

--- client/vite-project/src/components/back-button/BackButton.jsx
+++ client/vite-project/src/components/back-button/BackButton.jsx
@@ -1,29 +1,26 @@
-import { useRef } from 'react';
 import { useHistoryContext } from '../../hooks/useHistoryContext';
 import './BackButton.scss';
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const BackButton = () => {
-    const { location } = useHistoryContext();
+    const { routes } = useHistoryContext();
     const navigate = useNavigate();
 
-    let locationRef = useRef(1);
+    const location = useLocation();
+    const currentLocation = location.pathname;
+    //find the index of the current location in the routes []
+    //should be ok if i just specify the last index of the arr as I am now popping last item of routes when user goes back, but will refactor at later point
+    const currentRouteIndex = routes.indexOf(currentLocation);
+    const prevRouteIndex = currentRouteIndex - 1;
 
     const handleClick = () => {
-        // I know I can also use navigate(-1), but wanted to try and build my own BackButton functionality
-        // pass isBack in the state, so we can scroll to the item only if we are going back (and not opening quotes as a normal route in the nav)
-        navigate(location[locationRef.current], {
+
+        navigate(routes[prevRouteIndex], {
             state: {
                 isBack: true
             }
         });
-
-        // here i use locationRef.current as index that i mapped to array item, which is the path to which I want to go back
-        // i do not want to keep incrementing locationRef.current to increase number of items in location arr as result will be inaccurate
-        // then i also increment locationRef.current with 2 instead of 1, as when I go back a page the last route is added at position 1 of location arr, so the last route i want to go to will be 2 indexes down, instead of 1, as it's pushed by the newly added route (last route)
-        if (locationRef.current < location.length - 1) {
-            locationRef.current += 2;
-        }
     }
 
     return (
