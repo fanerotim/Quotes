21a4683041006eb82b91a012d0b159473846335f
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-23 14:18:54 +0300

    wrao the app inside scroll context provider, so we can consume it

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -5,15 +5,18 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import AuthContextProvider from './contexts/AuthContextProvider.jsx';
 import HistoryContextProvider from './contexts/HistoryContextProvider.jsx';
+import ScrollContextProvider from './contexts/ScrollContextProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
     <HistoryContextProvider>
-      <BrowserRouter>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </BrowserRouter>
+      <ScrollContextProvider>
+        <BrowserRouter>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </BrowserRouter>
+      </ScrollContextProvider>
     </HistoryContextProvider>
   </AuthContextProvider>
 
