056b8b9d717f69f2f41f4681beb8752d8aaa0bfc
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-24 17:06:01 +0300

    implement DeleteModal component and use props passed to it by DeleteButton

--- client/vite-project/src/components/delete-modal/DeleteModal.jsx
+++ client/vite-project/src/components/delete-modal/DeleteModal.jsx
@@ -1,13 +1,23 @@
 import './DeleteModal.scss'
 
-const DeleteModal = () => {
-    
+const DeleteModal = ({ isOpen, onDelete, toggleIsOpen }) => {
+
     return (
-        <section className='delete-modal-container'>
-            <div className='delete-modal-wrapper'>
-                 <h1>Are you sure you want to delete this quote?</h1>
-            </div>
-        </section>
+        isOpen && (
+            <section className='delete-modal-container'>
+                <div className='delete-modal-wrapper'>
+                    <h1>Are you sure you want to delete this quote?</h1>
+                    <button
+                        onClick={onDelete}>
+                        Yes
+                    </button>
+                    <button
+                        onClick={toggleIsOpen}>
+                        No
+                    </button>
+                </div>
+            </section>
+        )
     )
 }
 
