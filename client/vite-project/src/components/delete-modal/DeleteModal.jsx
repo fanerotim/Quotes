a75efd5fef40cde6a872b22867613f0f4ed25800
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-24 14:47:35 +0300

    create DeleteModal component

---
+++ client/vite-project/src/components/delete-modal/DeleteModal.jsx
@@ -0,0 +1,14 @@
+import './DeleteModal.scss'
+
+const DeleteModal = () => {
+    
+    return (
+        <section className='delete-modal-container'>
+            <div className='delete-modal-wrapper'>
+                 <h1>Are you sure you want to delete this quote?</h1>
+            </div>
+        </section>
+    )
+}
+
+export default DeleteModal;
