714c12466126fd804118b6fb267619743cf4bda7
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-25 18:27:56 +0300

    complete logic that is responsible for handling update password functionality - handling states - error, isLoading, isSubmitted
    it will refactored and improved to reduce number of states, but committing as is for now

--- client/vite-project/src/hooks/useUpdatePassword.js
+++ client/vite-project/src/hooks/useUpdatePassword.js
@@ -1,29 +1,77 @@
 import { useState } from "react";
+import useUpdatePasswordRequest from "./useUpdatePasswordRequest";
+import validateInputs from "../utils/validateInputs";
+import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from 'react-router-dom';
 
 const useUpdatePassword = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    // refactor using a status that combines the below states
+    // possible states: idle, submitted, loading, error
+    const [isSubmitted, setIsSubmitted] = useState(false)
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { updatePassword } = useUpdatePasswordRequest();
+    const { dispatch } = useAuthContext();
+    const navigate = useNavigate();
 
     const toggleIsOpen = () => {
         setIsOpen((isOpen) => !isOpen)
     }
 
-    // TODO: Send an actual request to the server to update the password
-    const handleSubmit = (e, values) => {
+    const handleSuccessfulUpdate = () => {
+        // show success message if we got to this fn
+        setIsSubmitted(true);
+
+        // hide form
+        setIsOpen(false);
+        
+        // set timeout, so user can have some time to read the message and then proceed with logic
+        const timeout = setTimeout(() => {
+            // logout user as they changed their password
+            dispatch({
+                type: 'LOGOUT'
+            })
+
+            // clear timeout
+            clearTimeout(timeout);
+            // navigate user to login page again, so they can login
+            navigate('/users/login');
+        }, 5000)
+    }
+
+    const handleSubmit = async (e, values) => {
         e.preventDefault();
-        console.log(values);
-        setIsSubmitted((isSubmitted) => !isSubmitted)
-        setIsOpen((isOpen) => !isOpen)
-        setTimeout(() => {
-            setIsSubmitted((isSubmitted) => !isSubmitted)
-        }, 3000)
+        // reset error if there was one in the prev request
+        setError(null);
+
+        try {
+            // validate inputs - this needs to be written properly
+            validateInputs(values);
+            // set is loading to true (disable submit button)
+            setIsLoading(true);
+            // make request to change password
+            await updatePassword(values);
+            // handle success by calling this function
+            handleSuccessfulUpdate();
+        } catch (err) {
+            setError(err.message);
+            throw err.message;
+        } finally {
+            // in any case - success or error - form no longer loads, so reset status
+            setIsLoading(false);
+        }
     }
 
     return {
         handleSubmit,
         toggleIsOpen,
         isOpen,
-        isSubmitted
+        isSubmitted,
+        error,
+        isLoading
     }
 }
 
