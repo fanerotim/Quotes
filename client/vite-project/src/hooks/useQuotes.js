52302bb4408878bce639bfc14aacf3d486ef8f1f
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-26 18:42:14 +0300

    create a new useQuotes hook that handles business logic

--- client/vite-project/src/hooks/useQuotes.js
+++ client/vite-project/src/hooks/useQuotes.js
@@ -1,28 +1,51 @@
-import http from "../requester/http";
-import { useAuthContext } from "./useAuthContext";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useScroll from "./useScroll";
+import useScrollContext from "./useScrollContext";
+import { useLocation } from "react-router-dom";
+import useRequestMoreQuotes from "./useRequestMoreQuotes";
 
 const useQuotes = () => {
+    const [quotes, setQuotes] = useState([]);
+    const { requestQuotes } = useRequestMoreQuotes();
+    const offset = 0;
+    const limit = 10;
 
-    const { dispatch } = useAuthContext();
+    const { createMap, scrollToItem } = useScroll();
+    const mapRefs = createMap();
+    // get scroll context data: item id and setter fn
+    const { itemId, updateItemId } = useScrollContext();
 
-    const getAllQuotes = async () => {
+    const navigate = useNavigate();
+    // access isBack prop (provided by back button) - use optional chaining as normally this prop does not exist;
+    const isBack = useLocation().state?.isBack;
 
-        try {
-            const result = await http.get(import.meta.env.VITE_BASE_URL);
-            return result;
-        } catch (err) {
-            // TODO: ADD ERROR HANDLING
-            dispatch({
-                type: 'LOGOUT'
-            })
-            // return(err)
-        }
+    const clickHandler = (id) => {
+        // update scroll context's itemId upon route change (so we know where to go back to afterwards)
+        updateItemId(id)
+        navigate(`/quotes/${id}`)
     }
+
+    // TODO: TOO MANY RERENDERS; FIX THIS;
+    useEffect(() => {
+
+        (async () => {
+            const result = await requestQuotes(offset, limit);
+            setQuotes(result);
+
+            // scroll to quote that was accessed before if user is clicking back button
+            if (isBack) {
+                scrollToItem(itemId);
+            }
+        })();
+
+    }, [])
 
     return {
-        getAllQuotes
+        quotes,
+        mapRefs,
+        clickHandler
     }
-
 }
 
 export default useQuotes;
