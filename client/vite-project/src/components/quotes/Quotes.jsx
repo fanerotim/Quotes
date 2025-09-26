08ed5ac401c73558468e76220b9170070bfcfc76
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-26 18:41:58 +0300

    remove all business logic out from Quotes component

--- client/vite-project/src/components/quotes/Quotes.jsx
+++ client/vite-project/src/components/quotes/Quotes.jsx
@@ -1,45 +1,15 @@
 import './Quotes.scss'
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import useQuotes from '../../hooks/useQuotes';
 import QuoteCard from '../quote-card/QuoteCard';
-import useScroll from '../../hooks/useScroll';
-import useScrollContext from '../../hooks/useScrollContext';
-import { useLocation } from 'react-router-dom';
+import LoadMoreQuotes from '../load-more-quotes/LoadMoreQuotes';
+import useQuotes from '../../hooks/useQuotes';
 
 const Quotes = () => {
-    const [quotes, setQuotes] = useState([]);
-    const { getAllQuotes } = useQuotes();
 
-    const { createMap, scrollToItem } = useScroll();
-    const mapRefs = createMap();
-    // get scroll context data: item id and setter fn
-    const { itemId, updateItemId } = useScrollContext();
-
-    const navigate = useNavigate();
-    // access isBack prop (provided by back button) - use optional chaining as normally this prop does not exist;
-    const isBack = useLocation().state?.isBack;
-
-    const clickHandler = (id) => {
-        // update scroll context's itemId upon route change (so we know where to go back to afterwards)
-        updateItemId(id)
-        navigate(`/quotes/${id}`)
-    }
-
-    // TODO: TOO MANY RERENDERS; FIX THIS;
-    useEffect(() => {
-
-        (async () => {
-            const result = await getAllQuotes();
-            setQuotes(result);
-
-            // scroll to quote that was accessed before if user is clicking back button
-            if (isBack) {
-                scrollToItem(itemId);
-            }
-        })();
-
-    }, [])
+    const { 
+        quotes, 
+        mapRefs, 
+        clickHandler 
+    } = useQuotes();
 
     return (
         <section className='quotes-container'>
@@ -52,10 +22,11 @@ const Quotes = () => {
                             mapRefs={mapRefs}
                             key={quote.id}
                             quote={quote}
-                            onCardClick={clickHandler} />
+                            onCardClick={() => clickHandler(quote.id)} />
                     ))}
                 </ul>)
                 : <h1>Currently we do not have quotes to display</h1>}
+            <LoadMoreQuotes />
         </section>
     )
 }
