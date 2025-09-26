7640c85724305d85c013e437433fbdf322241a28
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-26 22:24:42 +0300

    complete initial functionality of useLoadMoreQuotes custom hook
    it still has bugs to clear and refactor code, but main functionality works

--- client/vite-project/src/hooks/useLoadMoreQuotes.js
+++ client/vite-project/src/hooks/useLoadMoreQuotes.js
@@ -1,20 +1,33 @@
 import useRequestMoreQuotes from "./useRequestMoreQuotes";
-import useRequestMoreQuotes from "./useRequestMoreQuotes";
+import useQuoteContext from '../hooks/useQuoteContext';
+import { useState } from "react";
 
 const useLoadMoreQuotes = () => {
 
-    const { requestMoreQuotes } = useRequestMoreQuotes();
+    const { requestQuotes } = useRequestMoreQuotes();
+    const { quotes, updateQuotes } = useQuoteContext();
+    const [hasMore, setHasMore] = useState(true);
+
+    const offset = quotes.length;
+    const limit = 10;
 
     const clickHandler = async () => {
+
         try {
-            const nextQuotes = await requestMoreQuotes()
-        } catch(err) {
+            const nextQuotes = await requestQuotes(offset, limit);
+
+            if (nextQuotes.length < 1) {
+                setHasMore(false);
+            }
+            updateQuotes(nextQuotes);
+        } catch (err) {
             throw err.message
         }
     }
 
     return {
-        clickHandler
+        clickHandler,
+        hasMore
     }
 }
 
