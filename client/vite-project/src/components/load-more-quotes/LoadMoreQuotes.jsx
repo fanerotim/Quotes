9b23a0605355eed2cb391d97bf2246284801bd99
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-26 22:23:37 +0300

    import hasMore state to disable button in case everything has been fetched

--- client/vite-project/src/components/load-more-quotes/LoadMoreQuotes.jsx
+++ client/vite-project/src/components/load-more-quotes/LoadMoreQuotes.jsx
@@ -3,11 +3,15 @@ import useLoadMoreQuotes from '../../hooks/useLoadMoreQuotes';
 
 const LoadMoreQuotes = () => {
 
-    const { clickHandler } = useLoadMoreQuotes();
+    const {
+        hasMore,
+        clickHandler }
+        = useLoadMoreQuotes();
 
     return (
         <button
             onClick={clickHandler}
+            disabled={!hasMore}
             className='load-more-btn'>
             Load More Quotes
         </button>
