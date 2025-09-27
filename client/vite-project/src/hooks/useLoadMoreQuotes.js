fb34aabb4b4cfdf0f2aa84560feb2b57cb134e32
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-27 20:55:24 +0300

    add conditional chaining to fix a bug which happens in quotes is an empty []

--- client/vite-project/src/hooks/useLoadMoreQuotes.js
+++ client/vite-project/src/hooks/useLoadMoreQuotes.js
@@ -1,26 +1,29 @@
 import useRequestMoreQuotes from "./useRequestMoreQuotes";
 import useQuoteContext from '../hooks/useQuoteContext';
 import { useState } from "react";
+import useInitialQuotes from '../hooks/useInitialQuotes';
 
 const useLoadMoreQuotes = () => {
 
     const { requestQuotes } = useRequestMoreQuotes();
     const { quotes, updateQuotes } = useQuoteContext();
     const [hasMore, setHasMore] = useState(true);
+    const { setQuotesinLocalStorage } = useInitialQuotes();
 
-    const offset = quotes.length;
+    const offset = quotes?.length;
     const limit = 10;
 
     const clickHandler = async () => {
 
         try {
             const nextQuotes = await requestQuotes(offset, limit);
-
+            const updatedLocalStorageQuotes = setQuotesinLocalStorage(nextQuotes);
+            updateQuotes(updatedLocalStorageQuotes);
             // think about and improve this logic - think about isLoading state. should i use reducer?
             if (nextQuotes.length < limit) {
                 setHasMore(false);
             }
-            updateQuotes(nextQuotes);
+
         } catch (err) {
             throw err.message
         }
