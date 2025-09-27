915e724b791fb473ff58ec8f6784b2fcd120bfe0
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-27 20:53:59 +0300

    update QuoteContext to set quotes state by reading localStorage
    there is a bug where initial quotes overwrite already set in localStorage quotes, which i am debugging, but wanted to commit

--- client/vite-project/src/contexts/QuoteContextProvider.jsx
+++ client/vite-project/src/contexts/QuoteContextProvider.jsx
@@ -1,18 +1,29 @@
 import { createContext, useState } from "react";
+import { useEffect } from "react";
+import useInitialQuotes from "../hooks/useInitialQuotes";
+
+const { getInitialQuotes } = useInitialQuotes();
+const initialQuotes = getInitialQuotes();
 
 export const QuoteContext = createContext();
 
 export const QuoteContextProvider = ({ children }) => {
 
-    const [quotes, setQuotes] = useState([]);
+    // everything works now apart from setting initial quotes. it was overriding setting new quotes;
+    const [quotes, setQuotes] = useState(initialQuotes);
 
     const updateQuotes = (newQuotes) => {
-        setQuotes((oldQuotes) => [...oldQuotes, ...newQuotes]);
+        setQuotes(newQuotes);
     }
+
+    useEffect(() => {
+        const quotesInLocalStorage = JSON.parse(localStorage.getItem('quotes'));
+        setQuotes(quotesInLocalStorage);
+    }, [])
 
     return (
         <QuoteContext.Provider
-            value={{quotes, updateQuotes}}>
+            value={{ quotes, updateQuotes }}>
             {children}
         </QuoteContext.Provider>
     )
