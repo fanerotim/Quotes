37010c0185df1bcaa28c2a135895522247d1e785
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-29 18:32:44 +0300

    import a method that returns a boolean depending on whether we have more quotes in db

--- client/vite-project/src/components/load-more-quotes/LoadMoreQuotes.jsx
+++ client/vite-project/src/components/load-more-quotes/LoadMoreQuotes.jsx
@@ -1,17 +1,19 @@
 import './LoadMoreQuotes.scss'
 import useLoadMoreQuotes from '../../hooks/useLoadMoreQuotes';
+import useHasMoreQuotes from '../../hooks/useHasMoreQuotes';
 
 const LoadMoreQuotes = () => {
 
     const {
-        hasMore,
         clickHandler
     } = useLoadMoreQuotes();
+
+    const { getHasMoreStatus } = useHasMoreQuotes();
 
     return (
         <button
             onClick={clickHandler}
-            disabled={!hasMore}
+            disabled={!getHasMoreStatus()}
             className='load-more-btn'>
             Load More Quotes
         </button>
