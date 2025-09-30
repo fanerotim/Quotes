9810e3cbb2a02764573c6c4509bf900f38527873
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-09-30 22:17:15 +0300

    create a custom hook that makes api calls to request searched data / quotes

---
+++ client/vite-project/src/hooks/useRequestSearch.js
@@ -0,0 +1,20 @@
+import http from '../requester/http';
+
+const useRequestSearch = () => {
+
+    const requestSearch = async (searchText) => {
+        try {
+            const searchedQuotes = await http.post(`${import.meta.env.VITE_BASE_URL}/search-quotes`, searchText);
+            return searchedQuotes;
+        } catch (err) {
+            console.error(err.message);
+            return err;
+        }
+    }
+
+    return {
+        requestSearch
+    }
+}
+
+export default useRequestSearch;
