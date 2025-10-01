95e61e8491f64193ff81e2119d9d34ea963ecb3d
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-01 21:34:25 +0300

    request quote data and then pass it to view to be rendered dynamically
    app is not yet hosted, so I cannot test if this all works now and will come back to it as soon as i deploy it. prob tomorrow, for testing purposes. otherwise, not ready for deployment

--- server/controllers/botController.js
+++ server/controllers/botController.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
+const quoteService = require('../services/quoteService');
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     const userAgent = req.headers['user-agent'];
     const { id } = req.params;
-    res.render('bot', { id });
+
+    try {
+        const result = await quoteService.getQuote(id);
+        const quoteDetails = result[0];
+        res.render('bot', { id, quoteDetails });
+    } catch (err) {
+        console.error(err.message);
+        throw err;
+    }
+
 })
 
 module.exports = router;
