7ea93a9e39787eeb538b2c3c9a9e0501b33643fc
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-02 22:39:40 +0300

    add some basic styles to cut text in quote card if it's longer than 100 chars
    styling work to start very soon - almost all essential functionality that I wanted to have is completed. finishing off like tomorrow and starting to style the app.
    
    reviewing performance / renders and tests will be last as I want to deploy and show this app already

--- client/vite-project/src/components/quote-card/QuoteCard.jsx
+++ client/vite-project/src/components/quote-card/QuoteCard.jsx
@@ -2,16 +2,21 @@ import './QuoteCard.scss';
 
 const QuoteCard = ({ quote, mapRefs, onCardClick }) => {
 
+    // format quote text to not show in its entirety if it's too long
+    const quoteText = quote.text.length > 100 ? quote.text.substring(0, 100) + '...' : quote.text;
+
     return (
         <article
             ref={(node) => {
                 mapRefs.set(quote.id, node)
             }}>
-            <li>{quote.text}</li>
-            <h2>{quote.author}</h2>
+            <li>{quoteText}</li>
+            
             <button onClick={() => onCardClick(quote.id)}>
-                Show details
+                Show more
             </button>
+
+            <h2>{quote.author}</h2>
         </article>
     )
 }
