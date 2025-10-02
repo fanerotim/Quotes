79ba5c6c6756a654e96e6944f424eac257e49226
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-02 16:21:16 +0300

    add isHovered state to control facebook icon's styles

--- client/vite-project/src/components/social-sharing-buttons/SocialSharingButtons.jsx
+++ client/vite-project/src/components/social-sharing-buttons/SocialSharingButtons.jsx
@@ -1,22 +1,26 @@
 import { FacebookShareButton, FacebookIcon } from 'react-share';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const SocialSharingButtons = () => {
 
     const location = useLocation();
-    console.log(location);
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <div className='social__media__buttons'>
-                <FacebookShareButton
-                    url={`${import.meta.env.VITE_BASE_URL}${location.pathname}`}
-                >
-                    <FacebookIcon
-                        className='fb__button'
-                        size={32}
-                        round={true}
-                    />
-                </FacebookShareButton>
+            <FacebookShareButton
+                url={`${import.meta.env.VITE_BASE_URL}${location.pathname}`}
+                resetButtonStyle={true}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                <FacebookIcon
+                    size={32}
+                    round={true}
+                    iconFillColor={isHovered ? 'rgb(26, 26, 26)' : 'white'}
+                />
+            </FacebookShareButton>
         </div>
     )
 }
