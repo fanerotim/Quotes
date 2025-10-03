57c161c43ecfd43a2c933aadffec1a3d147d5cf1
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-03 14:43:08 +0300

    add custom hook responsible for api calls related to liking a quote and checking if a quote is already liked

---
+++ client/vite-project/src/hooks/useLikeRequests.js
@@ -0,0 +1,30 @@
+import http from '../requester/http';
+
+const useLikeRequests = () => {
+
+    const addLike = async (quoteId) => {
+        try {
+            const likeResult = await http.post(`${import.meta.env.VITE_BASE_URL}/add-like`, { quoteId });
+            return likeResult;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    const checkIfAlreadyLiked = async (quoteId) => {
+        
+        try {
+            const result = await http.post(`${import.meta.env.VITE_BASE_URL}/has-liked-quote`, { quoteId });
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    return {
+        addLike,
+        checkIfAlreadyLiked
+    }
+}
+
+export default useLikeRequests;
