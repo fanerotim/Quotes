313696916ddae671fef33faf82bf6016231b61bd
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-03 19:22:11 +0300

    create a useQuoteDetails custom hook that manages state and handles related to QuoteDetails copponent and manages shared state accessed by LikeButton and LikesCountIcon components

---
+++ client/vite-project/src/hooks/useQuoteDetails.js
@@ -0,0 +1,42 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useGetQuote from "./useGetQuote";
+import { useAuthContext } from "./useAuthContext";
+import useLikeRequests from "./useLikeRequests";
+
+const useQuoteDetails = () => {
+    const { quoteId } = useParams();
+    const { getQuote } = useGetQuote();
+    const [quote, setQuote] = useState(null);
+
+    const { auth } = useAuthContext();
+    const isOwner = auth && quote && auth.id === quote.ownerId ? true : false;
+    const canLike = auth && !isOwner;
+
+    const { getLikesCount } = useLikeRequests();
+    const [likesCount, setLikesCount] = useState(0);
+
+    const updateLikeCount = () => {
+        setLikesCount((prev) => prev + 1);
+    }
+
+    useEffect(() => {
+        getQuote(quoteId)
+            .then(data => setQuote(data))
+            .catch(err => console.error(err));
+
+        getLikesCount(quoteId)
+            .then(data => setLikesCount(data.length))
+            .catch(err => console.error(err));
+    }, [])
+
+    return {
+        quote,
+        likesCount,
+        isOwner,
+        canLike,
+        updateLikeCount
+    }
+}
+
+export default useQuoteDetails;
