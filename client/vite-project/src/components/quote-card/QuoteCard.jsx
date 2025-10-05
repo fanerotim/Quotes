f67e6e4fab96f1d5758d99714624701ede010b73
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-05 17:42:12 +0300

    add initial styles of quote card, adjust 'more' button, add some styles to it, create a textLength variable that controls text length inside the card

--- client/vite-project/src/components/quote-card/QuoteCard.jsx
+++ client/vite-project/src/components/quote-card/QuoteCard.jsx
@@ -1,22 +1,33 @@
-import './QuoteCard.scss';
+import styles from './QuoteCard.module.scss';
+import { Link } from 'react-router-dom'
 
 const QuoteCard = ({ quote, mapRefs, onCardClick }) => {
 
     // format quote text to not show in its entirety if it's too long
-    const quoteText = quote.text.length > 100 ? quote.text.substring(0, 100) + '...' : quote.text;
+    const textLength = 80;
+    const quoteText = quote.text.length > textLength ? quote.text.substring(0, textLength) + '...' : quote.text;
 
     return (
         <article
+            onClick={() => onCardClick(quote.id)}
+            className={styles.card}
             ref={(node) => {
                 mapRefs.set(quote.id, node)
             }}>
-            <li>{quoteText}</li>
-            
-            <button onClick={() => onCardClick(quote.id)}>
-                Show more
-            </button>
+            <li
+                className={styles.card__text}>
+                {quoteText}
+                {quoteText.length > textLength && <Link className={styles.card__more__btn}>More</Link>}
+            </li>
 
-            <h2>{quote.author}</h2>
+            {/* <button onClick={() => onCardClick(quote.id)}>
+                 Show more
+            </button> */}
+
+            <h2
+                className={styles.card__author}>
+                {quote.author}
+            </h2>
         </article>
     )
 }
