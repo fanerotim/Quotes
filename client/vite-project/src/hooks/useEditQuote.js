1464ba9239fd17580a6d6bdceaae90b1cbb92a6e
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-06 09:06:24 +0300

    create a new custom hook useEditQuote that handles everything related to editing a quote - state, calling the custom hook making api calls, providing EdiQuote component with initial quote values, calling hook if we get a 401 error navigating on success
    not yet showing a error toaster - this will be done in due course

--- client/vite-project/src/hooks/useEditQuote.js
+++ client/vite-project/src/hooks/useEditQuote.js
@@ -1,36 +1,57 @@
-import http from "../requester/http";
-import { useState } from "react";
-import useLogoutOn401Error from "./useLogoutOn401Error";
-import validateInputs from "../utils/validateInputs";
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import useEditQuote from './useRequestEditQuote';
+import useGetQuote from './useGetQuote';
+import useLogoutOn401Error from './useLogoutOn401Error';
+import useLocalStorageQuotes from './useLocalStorageQuotes';
+import validateInputs from '../utils/validateInputs';
 
-const useEditQuote = () => {
+const useEdit = () => {
+    const [quote, setQuote] = useState(null);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    
+    const navigate = useNavigate();
+    const { quoteId } = useParams();
+    
+    const { getQuote } = useGetQuote();
+    const { edit } = useEditQuote();
     const { logoutOn401 } = useLogoutOn401Error();
+    const { updateLocalStorageQuotesAfterEdit } = useLocalStorageQuotes();
 
-    const edit = async (values, quoteId) => {
+
+    useEffect(() => {
+        // get data of quote we want to edit
+        getQuote(quoteId)
+            .then(data => setQuote(data))
+            .catch(err => console.error(err))
+    }, []);
+
+    const handleSubmit = async (e, values) => {
+        e.preventDefault();
         setError(null);
 
         try {
-            // validate input / throws error on empty field
             validateInputs(values);
             setIsLoading(true);
-            await http.put(`${import.meta.env.VITE_BASE_URL}/edit-quote/${quoteId}`, values);
+            await edit(values, quoteId);
+            updateLocalStorageQuotesAfterEdit({ id: quoteId, ...values });
+            navigate(`/quotes/${quoteId}`)
         } catch (err) {
-            // check if error is 401 to logout the user
+            // if err, check for 401 and logout user if so
             logoutOn401(err);
             setError(err.message);
-            throw error;
         } finally {
             setIsLoading(false);
         }
     }
 
     return {
-        edit,
+        handleSubmit,
+        quote,
         error,
         isLoading
     }
 }
 
-export default useEditQuote;
+export default useEdit;
