1036f503f67c1ded258cc3ff275885b0398f4a31
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-12 20:55:11 +0300

    add initial basic styles to reset password page and align it to be consistent with other pages with forms design

--- client/vite-project/src/components/reset-password/ResetPassword.jsx
+++ client/vite-project/src/components/reset-password/ResetPassword.jsx
@@ -1,4 +1,4 @@
-import './ResetPassword.scss';
+import styles from './ResetPassword.module.scss';
 import useResetPassword from '../../hooks/useResetPassword';
 import useForm from '../../hooks/useForm';
 
@@ -18,20 +18,59 @@ const ResetPassword = () => {
     } = useResetPassword();
 
     return (
-        <section>
-            <h1>Reset your password</h1>
-            {!isSubmitted && <form
-                onSubmit={(e) => handleSubmit(e, values)}>
-                <label>Enter your email</label>
-                <input
-                    value={values.email}
-                    name='email'
-                    onChange={handleChange}
-                    type="email"
-                />
-                <br />
-                <button disabled={isLoading}>Submit</button>
-            </form>}
+        <section
+            className={styles.reset__password__container}
+        >
+
+            <div
+                className={styles.reset__password__text__container}
+            >
+                <h1
+                    className={styles.reset__password__text__heading}
+                >
+                    Reset
+                </h1>
+                <h2
+                    className={styles.reset__password__text__subheading}
+                >
+                    Fill in the form and we'll email you a new one
+                </h2>
+            </div>
+
+            {!isSubmitted &&
+
+                <form
+                    onSubmit={(e) => handleSubmit(e, values)}
+                    className={styles.reset__password__form}
+                >
+
+                    <div
+                        className={styles.form__data}
+                    >
+                        <label
+                            className={styles.reset__password__form__label}
+                        >
+                            Email address
+                        </label>
+                        <input
+                            onChange={handleChange}
+                            className={styles.reset__password__form__input}
+                            value={values.email}
+                            placeholder='Enter your email'
+                            name='email'
+                            type="email"
+                            required
+                        />
+                    </div>
+
+                    <button
+                        className={styles.reset__password__form__submit__button}
+                        disabled={isLoading}
+                    >
+                        Submit
+                    </button>
+                </form>}
+
             {error && <p>{error}</p>}
             {isSubmitted && <h2>Password updated successfully. Please check your email and log in again.</h2>}
         </section>
