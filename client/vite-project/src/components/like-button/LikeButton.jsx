61de5025d79e418d715c4ca4deb152b27dc26c94
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-13 16:13:50 +0300

    add styles to like button. import an icon and show text conditionally to inform user that they can like/ have liked a quote

--- client/vite-project/src/components/like-button/LikeButton.jsx
+++ client/vite-project/src/components/like-button/LikeButton.jsx
@@ -1,21 +1,36 @@
-import './LikeButton.scss';
+import styles from './LikeButton.module.scss';
 import useLike from '../../hooks/useLike';
+import { AiOutlineLike } from "react-icons/ai";
 
-const LikeButton = ({updateLikeCount}) => {
+const LikeButton = ({ updateLikeCount }) => {
 
     const { handleLike, hasLiked } = useLike();
 
     return (
-        <div>
+        <div
+            className={styles.like__button__container}
+        >
             <button
                 onClick={(e) => {
                     updateLikeCount();
                     handleLike(e);
                 }}
+                className={styles.like__button}
                 disabled={hasLiked}
             >
-                Like
+                <AiOutlineLike />
+
             </button>
+
+
+            <p
+                className={styles.user__has__liked__message}
+            >
+                {hasLiked
+                    ? 'You have already liked this quote'
+                    : 'Give a like'}
+            </p>
+
         </div>
     )
 }
