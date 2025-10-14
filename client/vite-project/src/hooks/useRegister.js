532d67d9da3638f6137bedd287778ccc5db6a029
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-14 20:51:05 +0300

    consume form states from useFormStates hook and return them to Register component

--- client/vite-project/src/hooks/useRegister.js
+++ client/vite-project/src/hooks/useRegister.js
@@ -1,14 +1,18 @@
 import validateInputs from "../utils/validateInputs";
 import useRegisterRequest from "./useRegisterRequest";
 import { useNavigate } from "react-router-dom";
+import useFormStates from "./useFormStates";
 
 const useRegister = () => {
 
     const { register } = useRegisterRequest();
     const navigate = useNavigate();
+    const {error, isLoading, success, updateState} = useFormStates();
 
     const submitHandler = async (e, values) => {
         e.preventDefault();
+        // reset error upon next api request
+        updateState('SET_ERROR');
 
         // additional check to make sure passwords match / this logic no yet added to my validateInputs function
         if (values.password !== values.rePassword) {
@@ -17,17 +21,25 @@ const useRegister = () => {
         }
 
         try {
+            // set loading to true before making the api request
+            updateState('SET_LOADING');
             validateInputs(values)
             const token = await register(values);
+            // set success to true and reset error and isLoading states after token is returned
+            updateState('SET_SUCCESS');
             navigate('/users/login')
             return token;
         } catch (err) {
+            updateState('SET_ERROR', err);
             throw err;
         }
     }
 
     return {
-        submitHandler
+        submitHandler,
+        error,
+        isLoading,
+        success
     }
 }
 
