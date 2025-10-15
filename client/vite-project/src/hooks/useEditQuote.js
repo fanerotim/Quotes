a0bfaae7d55124f2c4d9bcdd464aff28996059fc
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-15 14:50:36 +0300

    consume useFormStates hook inside useEditQuote and return form states to EditQuote component

--- client/vite-project/src/hooks/useEditQuote.js
+++ client/vite-project/src/hooks/useEditQuote.js
@@ -5,35 +5,51 @@ import useGetQuote from './useGetQuote';
 import useLogoutOn401Error from './useLogoutOn401Error';
 import useLocalStorageQuotes from './useLocalStorageQuotes';
 import validateInputs from '../utils/validateInputs';
+import useFormStates from './useFormStates';
 
 const useEditQuote = () => {
     const [quote, setQuote] = useState(null);
-    
+
     const navigate = useNavigate();
     const { quoteId } = useParams();
-    
+
     const { getQuote } = useGetQuote();
     const { edit } = useEditQuoteRequest();
     const { logoutOn401 } = useLogoutOn401Error();
     const { updateLocalStorageQuotesAfterEdit } = useLocalStorageQuotes();
+    const { isLoading, error, success, updateState } = useFormStates();
 
 
     useEffect(() => {
+        // set isLoading to true whilst data is being fetched
+        updateState('SET_LOADING');
         // get data of quote we want to edit
         getQuote(quoteId)
-            .then(data => setQuote(data))
+            .then(data => {
+                // set success to true / isLoading is being updated to false
+                updateState('SET_SUCCESS');
+                setQuote(data)
+            })
             .catch(err => console.error(err))
     }, []);
 
     const handleSubmit = async (e, values) => {
         e.preventDefault();
+        // reset error state as we'll be making a new api call
+        updateState('SET_ERROR');
 
         try {
+            // set isLoading to true again as a new api call is being made
+            updateState('SET_LOADING');
             validateInputs(values);
             await edit(values, quoteId);
+            // set success to true / isLoading to false as quote is edited / updated successfully
+            updateState('SET_SUCCESS');
             updateLocalStorageQuotesAfterEdit({ id: quoteId, ...values });
             navigate(`/quotes/${quoteId}`)
         } catch (err) {
+            // set error to true and pass err
+            updateState('SET_ERROR', err);
             // if err, check for 401 and logout user if so
             logoutOn401(err);
             throw err;
@@ -42,7 +58,9 @@ const useEditQuote = () => {
 
     return {
         handleSubmit,
-        quote
+        quote,
+        error,
+        isLoading
     }
 }
 
