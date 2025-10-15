0069df455a8bf5fdf3e256eae1413454188fefa4
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-15 14:16:16 +0300

    conditionally render Toaster and Loader based on isLoading and error states, which are now consumed from useAddQuote. Give Toaster and Loader place on the grid

--- client/vite-project/src/components/add-quote/AddQuote.jsx
+++ client/vite-project/src/components/add-quote/AddQuote.jsx
@@ -2,6 +2,8 @@ import styles from './AddQuote.module.scss'
 import useForm from '../../hooks/useForm';
 import useAddQuote from '../../hooks/useAddQuote';
 import { CATEGORIES } from '../../utils/genres';
+import Loader from '../loader/Loader';
+import Toaster from '../toaster/Toaster';
 
 const initialValues = {
     author: '',
@@ -12,7 +14,10 @@ const initialValues = {
 const AddQuote = () => {
 
     const { values, handleChange } = useForm(initialValues);
-    const { handleSubmit } = useAddQuote();
+    const { handleSubmit,
+        isLoading,
+        error
+    } = useAddQuote();
 
     return (
         <div className={styles.add__form__container}>
@@ -22,79 +27,94 @@ const AddQuote = () => {
                 <p className={styles.add__form__text__subheading}>Add a quote</p>
             </div>
 
-            <form
-                onSubmit={(e) => handleSubmit(e, values)}
-                className={styles.add__form}>
-
-                <section
-                    className={styles.add__form__form__data}
+            {isLoading
+                ?
+                <div
+                    className={styles.add__form__loader__container}
                 >
-                    <label
-                        className={styles.add__form__label}
-                    >
-                        Author
-                    </label>
-                    <input
-                        onChange={handleChange}
-                        value={values.author}
-                        className={styles.add__form__input}
-                        placeholder='Minimum 8 characters'
-                        name='author'
-                        type="text"
-                        required
-                        minLength={8} />
-                </section>
+                    <Loader />
+                </div>
+                :
+                <form
+                    onSubmit={(e) => handleSubmit(e, values)}
+                    className={styles.add__form}>
 
-                <section
-                    className={styles.add__form__form__data}
-                >
-                    <label
-                        className={styles.add__form__label}
+                    <section
+                        className={styles.add__form__form__data}
                     >
-                        Text
-                    </label>
-                    <textarea
-                        onChange={handleChange}
-                        value={values.text}
-                        className={styles.add__form__textarea}
-                        placeholder='Minimum 15 characters'
-                        name='text'
-                        type="text"
-                        required
-                        minLength={15} />
-                </section>
+                        <label
+                            className={styles.add__form__label}
+                        >
+                            Author
+                        </label>
+                        <input
+                            onChange={handleChange}
+                            value={values.author}
+                            className={styles.add__form__input}
+                            placeholder='Minimum 8 characters'
+                            name='author'
+                            type="text"
+                            required
+                            minLength={8} />
+                    </section>
 
-                <section
-                    className={styles.add__form__form__data}
-                >
-                    <label
-                        className={styles.add__form__label}
+                    <section
+                        className={styles.add__form__form__data}
                     >
-                        Category
-                    </label>
-                    <select
-                        onChange={handleChange}
-                        className={styles.add__form__select}
-                        name="category"
-                        required
-                    >
-                        {CATEGORIES.map(category => (
-                            <option
-                                key={category.value}
-                                value={category.value}
-                            >
-                                {category.name}
-                            </option>
-                        ))}
-                    </select>
-                </section>
+                        <label
+                            className={styles.add__form__label}
+                        >
+                            Text
+                        </label>
+                        <textarea
+                            onChange={handleChange}
+                            value={values.text}
+                            className={styles.add__form__textarea}
+                            placeholder='Minimum 15 characters'
+                            name='text'
+                            type="text"
+                            required
+                            minLength={15} />
+                    </section>
 
-                <button 
-                    className={styles.add__form__button}
-                >
-                    Submit
-                </button>
-            </form>
+                    <section
+                        className={styles.add__form__form__data}
+                    >
+                        <label
+                            className={styles.add__form__label}
+                        >
+                            Category
+                        </label>
+                        <select
+                            onChange={handleChange}
+                            className={styles.add__form__select}
+                            name="category"
+                            required
+                        >
+                            {CATEGORIES.map(category => (
+                                <option
+                                    key={category.value}
+                                    value={category.value}
+                                >
+                                    {category.name}
+                                </option>
+                            ))}
+                        </select>
+                    </section>
+
+                    <button
+                        className={styles.add__form__button}
+                    >
+                        Submit
+                    </button>
+                </form>
+            }
+
+            <div
+                className={styles.add__form__toaster__container}
+            >
+                <Toaster message={error?.message}/>
+            </div>
         </div>
     )
 }
