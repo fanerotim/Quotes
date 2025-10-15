66a83d62c17e3e64b3b867980e6f548fd533de61
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-15 14:50:55 +0300

    conditionally render Toaster and Loader inside QuoteDetails page based onform states

--- client/vite-project/src/components/edit-quote/EditQuote.jsx
+++ client/vite-project/src/components/edit-quote/EditQuote.jsx
@@ -3,13 +3,16 @@ import styles from './EditQuote.module.scss'
 import { CATEGORIES } from '../../utils/genres';
 import BackButton from '../back-button/BackButton';
 import useEditQuote from '../../hooks/useEditQuote';
-
+import Loader from '../loader/Loader';
+import Toaster from '../toaster/Toaster';
 
 const EditQuote = () => {
 
     const {
         handleSubmit,
-        quote
+        quote,
+        isLoading,
+        error
     } = useEditQuote();
 
     const { values, handleChange } = useForm(quote);
@@ -19,90 +22,108 @@ const EditQuote = () => {
             className={styles.edit__form__container}
         >
 
-            <div
-                className={styles.back__button__container}
-            >
-                <BackButton />
-            </div>
-
-            <div
-                className={styles.edit__form__text__container}
-            >
-                <h1
-                    className={styles.edit__form__text__container__heading}
+            {isLoading
+                ?
+                <div
+                    className={styles.edit__form__loader__container}
                 >
-                    Edit
-                </h1>
-            </div>
+                    <Loader />
 
-            <form
-                onSubmit={(e) => handleSubmit(e, values)}
-                className={styles.edit__form}
-            >
+                </div>
 
-                <section
-                    className={styles.form__data}
-                >
-                    <label
-                        className={styles.edit__form__label}
+                :
+                <>
+                    <div
+                        className={styles.back__button__container}
                     >
-                        Author
-                    </label>
-                    
-                    <input
-                        onChange={handleChange}
-                        className={styles.edit__form__input}
-                        value={values?.author}
-                        name='author'
-                    />
-                </section>
+                        <BackButton />
+                    </div>
 
-                <section
-                    className={styles.form__data}
-                >
-                    <label
-                        className={styles.edit__form__label}
+                    <div
+                        className={styles.edit__form__text__container}
                     >
-                        Text
-                    </label>
-                    <textarea
-                        onChange={handleChange}
-                        className={styles.edit__form__textarea}
-                        value={values?.text}
-                        name="text">
-                    </textarea>
-                </section>
+                        <h1
+                            className={styles.edit__form__text__container__heading}
+                        >
+                            Edit
+                        </h1>
+                    </div>
 
-                <section
-                    className={styles.form__data}
-                >
-                    <label
-                        className={styles.edit__form__label}
+                    <form
+                        onSubmit={(e) => handleSubmit(e, values)}
+                        className={styles.edit__form}
                     >
-                        Category
-                    </label>
-                    <select
-                        onChange={handleChange}
-                        className={styles.edit__form__select}
-                        name="category"
-                        // TODO: Fix this - find a better way to implement it
-                        // Using toLowerCase() as values in genres.js are written in lower case, so if I do not lowercase it, it does not get detected and updated in the UI.
-                        value={values?.category.toLowerCase()}>
-                        {CATEGORIES.map(category => (
-                            <option
-                                key={category.value}
-                                value={category.value}>
-                                {category.name}
-                            </option>
-                        ))}
-                    </select>
-                </section>
-                <button
-                    className={styles.edit__form__submit__button}
+
+                        <section
+                            className={styles.form__data}
+                        >
+                            <label
+                                className={styles.edit__form__label}
+                            >
+                                Author
+                            </label>
+
+                            <input
+                                onChange={handleChange}
+                                className={styles.edit__form__input}
+                                value={values?.author}
+                                name='author'
+                            />
+                        </section>
+
+                        <section
+                            className={styles.form__data}
+                        >
+                            <label
+                                className={styles.edit__form__label}
+                            >
+                                Text
+                            </label>
+                            <textarea
+                                onChange={handleChange}
+                                className={styles.edit__form__textarea}
+                                value={values?.text}
+                                name="text">
+                            </textarea>
+                        </section>
+
+                        <section
+                            className={styles.form__data}
+                        >
+                            <label
+                                className={styles.edit__form__label}
+                            >
+                                Category
+                            </label>
+                            <select
+                                onChange={handleChange}
+                                className={styles.edit__form__select}
+                                name="category"
+                                // TODO: Fix this - find a better way to implement it
+                                // Using toLowerCase() as values in genres.js are written in lower case, so if I do not lowercase it, it does not get detected and updated in the UI.
+                                value={values?.category.toLowerCase()}>
+                                {CATEGORIES.map(category => (
+                                    <option
+                                        key={category.value}
+                                        value={category.value}>
+                                        {category.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </section>
+                        <button
+                            className={styles.edit__form__submit__button}
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </>}
+
+                <div
+                    className={styles.edit__form__toaster__container}
                 >
-                    Submit
-                </button>
-            </form>
+                    <Toaster message={error?.message}/>
+                </div>
         </section>
     )
 }
