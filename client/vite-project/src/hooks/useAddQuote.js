56a0af7e285433c7a1da27d10d2be33fd6661879
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-16 18:36:07 +0300

    consume useSuccessModal's delayMs and wrap redirect in a setTimeout for now as I want to keep the success modal up for 5 seconds
    will extract the setTimeout logic in a custom hook, just still considering how to implement it

--- client/vite-project/src/hooks/useAddQuote.js
+++ client/vite-project/src/hooks/useAddQuote.js
@@ -4,13 +4,15 @@ import useHasMoreQuotes from "./useHasMoreQuotes";
 import useAddQuoteRequest from './useAddQuoteRequest';
 import { useNavigate } from 'react-router-dom';
 import useFormStates from './useFormStates';
+import useSuccessModal from "./useSuccessModal";
 
 const useAddQuote = () => {
 
     const { logoutOn401 } = useLogoutOn401Error();
     const { setHasMoreStatus } = useHasMoreQuotes();
     const { addQuote } = useAddQuoteRequest();
-    const { isLoading, error, updateState } = useFormStates();
+    const { isLoading, error, success, updateState } = useFormStates();
+    const { delayMs } = useSuccessModal();
     const navigate = useNavigate();
 
     const handleSubmit = async (e, values) => {
@@ -28,7 +30,10 @@ const useAddQuote = () => {
             // update local storage status of hasMore to true to make sure load more button is enabled again
             setHasMoreStatus(true);
             // redirect to quotes page after success
-            navigate('/quotes');
+            // for now i am writing a timeout here, as i want the modal to be visible for x amount of settings before redirecting the user
+            setTimeout(() => {
+                navigate('/quotes');
+            }, delayMs)
         } catch (err) {
             // if err > update error state with err.message
             updateState('SET_ERROR', err);
@@ -41,7 +46,8 @@ const useAddQuote = () => {
     return {
         handleSubmit,
         isLoading,
-        error
+        error,
+        success
     }
 }
 
