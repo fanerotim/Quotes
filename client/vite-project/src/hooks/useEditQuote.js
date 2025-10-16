b5d4b211a5bd0684455fe035f2fb06447c4c54b8
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-16 18:55:06 +0300

    consume useSuccessModal's delayMs and set a timeout before redirecting the user on successful edit as I want to keep the success modal for x amount of seconds
    also found a bug with SET_SUCCESS action - left a comment describe the issue and fix when refactoring review the code

--- client/vite-project/src/hooks/useEditQuote.js
+++ client/vite-project/src/hooks/useEditQuote.js
@@ -6,6 +6,7 @@ import useLogoutOn401Error from './useLogoutOn401Error';
 import useLocalStorageQuotes from './useLocalStorageQuotes';
 import validateInputs from '../utils/validateInputs';
 import useFormStates from './useFormStates';
+import useSuccessModal from './useSuccessModal';
 
 const useEditQuote = () => {
     const [quote, setQuote] = useState(null);
@@ -18,7 +19,7 @@ const useEditQuote = () => {
     const { logoutOn401 } = useLogoutOn401Error();
     const { updateLocalStorageQuotesAfterEdit } = useLocalStorageQuotes();
     const { isLoading, error, success, updateState } = useFormStates();
-
+    const { delayMs } = useSuccessModal();
 
     useEffect(() => {
         // set isLoading to true whilst data is being fetched
@@ -26,11 +27,14 @@ const useEditQuote = () => {
         // get data of quote we want to edit
         getQuote(quoteId)
             .then(data => {
-                // set success to true / isLoading is being updated to false
-                updateState('SET_SUCCESS');
+                // TODO: read below comment and fix this
+                // found a bug with SET_SUCCESS - it shows success modal as in this component i make two api calls, one to retrieve quote data and another one to update it. SET_SUCCESS shows modal when it should not, because i only loaded the quote data, no edit is made yet. 
+                // THERE IS NO ERROR, BUT THERE IS A BUG - THAT'S WHY I AM USING 'SET_ERROR' FOR NOW - ABOVE COMMENT CLARIFIES THE BUG
+                updateState('SET_ERROR');
                 setQuote(data)
             })
             .catch(err => console.error(err))
+            .finally()
     }, []);
 
     const handleSubmit = async (e, values) => {
@@ -45,8 +49,14 @@ const useEditQuote = () => {
             await edit(values, quoteId);
             // set success to true / isLoading to false as quote is edited / updated successfully
             updateState('SET_SUCCESS');
+            // update local storage as i am using it as a caching option to avoid making unnecesasry api calls
             updateLocalStorageQuotesAfterEdit({ id: quoteId, ...values });
-            navigate(`/quotes/${quoteId}`)
+
+            // setting this timeout as i want to show success modal for 5 seconds before redirecting the user
+            // this functionality will be extracted, but keeping it as it is for now until i decide how i want to extract it
+            setTimeout(() => {
+                navigate(`/quotes/${quoteId}`)
+            }, delayMs)
         } catch (err) {
             // set error to true and pass err
             updateState('SET_ERROR', err);
@@ -60,7 +70,8 @@ const useEditQuote = () => {
         handleSubmit,
         quote,
         error,
-        isLoading
+        isLoading,
+        success
     }
 }
 
