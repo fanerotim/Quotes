30bbcda253afd3fc7f25d6496c945cb672ebb887
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-17 21:09:50 +0300

    move all business logic into useDeleteModal hook. set form states, so we can show loader and success modal depending on states and return those states, so they can be used

--- client/vite-project/src/hooks/useDeleteModal.js
+++ client/vite-project/src/hooks/useDeleteModal.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
-import { useParams } from 'react-router-dom';
-import useDelete from '../hooks/useDelete'
+import { useParams, useNavigate } from 'react-router-dom';
+import useDeleteRequest from './useDeleteRequest';
+import useFormStates from "./useFormStates";
+import useLogoutOn401Error from "./useLogoutOn401Error";
+import useLocalStorageQuotes from "./useLocalStorageQuotes";
+import useSuccessModal from "./useSuccessModal";
 
 const useDeleteModal = () => {
 
     const [isOpen, setIsOpen] = useState(false);
-    const { deleteOne } = useDelete();
+    const { deleteOne } = useDeleteRequest();
+    const { logoutOn401 } = useLogoutOn401Error();
+    const { filterLocalStorageQuotesAfterDelete } = useLocalStorageQuotes();
+    const { isLoading, success, updateState } = useFormStates();
+    const { delayMs } = useSuccessModal();
     const { quoteId } = useParams();
+    const navigate = useNavigate();
 
     const toggleIsOpen = () => {
         setIsOpen((prevIsOpen) => !prevIsOpen);
@@ -14,13 +23,29 @@ const useDeleteModal = () => {
 
     const onDelete = async () => {
         // TODO: it is not very wise to hardcode the url 'delete-quote' in this case. refactor later
-        const deletedQuote = await deleteOne('delete-quote', { id: quoteId });
+
+        try {
+            updateState('SET_LOADING')
+            const deletedQuote = await deleteOne('delete-quote', { id: quoteId });
+            // remove deleted item from localStorage as /quotes page loads data from localStorage (used as cache to avoid unnecessary api calls)
+            filterLocalStorageQuotesAfterDelete(quoteId);
+            updateState('SET_SUCCESS');
+
+            setTimeout(() => {
+                navigate('/quotes')
+            }, delayMs)
+        } catch (err) {
+            logoutOn401(err)
+            // TODO: handle error state too
+        }
     }
 
     return {
         isOpen,
         toggleIsOpen,
-        onDelete
+        onDelete,
+        isLoading,
+        success
     }
 }
 
