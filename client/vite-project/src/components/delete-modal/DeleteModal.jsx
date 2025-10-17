84e41e54976f221ca3fb3d65596181e31f029b39
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-17 21:13:45 +0300

    render Loader and SuccessModal conditionally. Styles and better render will need to be improved, but functionality works for now, so moving on and when i refactor / write media queries for responsiveness will think it through
    spent a few hours trying to understand why form states (success and isLoading) were not updating. Want to move on for now finally made it work. Refactor and deeper understanding will come later on

--- client/vite-project/src/components/delete-modal/DeleteModal.jsx
+++ client/vite-project/src/components/delete-modal/DeleteModal.jsx
@@ -1,23 +1,65 @@
-import './DeleteModal.scss'
+import styles from './DeleteModal.module.scss';
+import Loader from '../loader/Loader';
+import SuccessModal from '../success-modal/SuccessModal';
 
-const DeleteModal = ({ isOpen, onDelete, toggleIsOpen }) => {
+const DeleteModal = ({ isOpen, onDelete, toggleIsOpen, isLoading, success }) => {
 
-    return (
-        isOpen && (
-            <section className='delete-modal-container'>
-                <div className='delete-modal-wrapper'>
-                    <h1>Are you sure you want to delete this quote?</h1>
-                    <button
-                        onClick={onDelete}>
-                        Yes
-                    </button>
-                    <button
-                        onClick={toggleIsOpen}>
-                        No
-                    </button>
+    if (success) {
+        return (
+            <section
+                className={styles.delete__modal__container}
+            >
+                <div
+                    className={styles.delete__modal__success_modal__wrapper}
+                >
+                    <SuccessModal />
                 </div>
             </section>
         )
+    }
+
+    return (
+        isOpen &&
+        <section
+            className={styles.delete__modal__container}
+        >
+
+            <section
+                className={styles.delete__modal__content__wrapper}
+            >
+                {isLoading
+                    ?
+                    <Loader />
+                    :
+                    <>
+                        <h1
+                            className={styles.delete__modal__heading}
+                        >
+                            Are you sure you want to delete this quote?
+
+                        </h1>
+
+                        <div
+                            className={styles.delete__modal__buttons__wrapper}
+                        >
+                            <button
+                                onClick={onDelete}
+                                className={styles.delete__modal__approve__btn}
+                            >
+                                Yes
+                            </button>
+                            <button
+                                onClick={toggleIsOpen}
+                                className={styles.delete__modal__reject__btn}
+                            >
+                                No
+                            </button>
+
+                        </div>
+                    </>
+                }
+            </section>
+        </section>
     )
 }
 
