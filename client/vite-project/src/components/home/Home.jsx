b6ffcb2c134739361ca26534084259a12325e49f
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-18 18:39:18 +0300

    import LastThreeQuotes component to /Home page
    no styling completed yet

--- client/vite-project/src/components/home/Home.jsx
+++ client/vite-project/src/components/home/Home.jsx
@@ -1,11 +1,15 @@
 import './Home.scss'
+import LastThreeQuotes from '../last-three-quotes/LastThreeQuotes';
 
 const Home = () => {
 
     return (
-        <>
+        <div
+            className='home-container'
+        >
             <h1 className='home-heading'>Welcome to My Quotes App</h1>
-        </>
+            <LastThreeQuotes/>
+        </div>
     )
 }
 
