8adf5659ef3c1f580191cbbb9cca762813039f7d
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-23 21:11:37 +0300

    create initial layout and styling of profile page
    a lot more work in terms of styling to be done, but at least we have some structure now

--- client/vite-project/src/components/user-profile/UserProfile.jsx
+++ client/vite-project/src/components/user-profile/UserProfile.jsx
@@ -2,50 +2,84 @@ import styles from './UserProfile.module.scss'
 import UpdatePassword from './update-password/UpdatePassword';
 import { Link } from 'react-router-dom';
 import useGetUserQuotes from '../../hooks/useGetUserQuotes';
+import Loader from '../loader/Loader';
 
 const UserProfile = () => {
 
     const { email, userQuotes } = useGetUserQuotes();
 
     return (
-        <section 
+        <section
             className={styles.page__wrapper}
         >
 
-            {email && 
-            <p
-                className={styles.profile__page__welcome__message}
-            >
-                Welcome, {email}!
-            </p>
-            }
+            {email &&
+                <div
+                    className={styles.profile__page__welcome__message__container}
+                >
 
-            <UpdatePassword />
+                    <p
+                        className={styles.profile__page__welcome__message}
+                    >
+                        Welcome,
+                    </p>
+
+                    <span
+                        className={styles.profile__page__welcome__message__email}
+                    >
+                        {email}
+                    </span>
+
+                    <p
+                        className={styles.profile__page__welcome__message__info}
+                    >
+                        Here you can review and edit your quotes, the quotes you liked and update your password.
+                    </p>
+                </div>}
+
+            <div
+                className={styles.profile__page__update__password__container}
+            >
+                <UpdatePassword />
+            </div>
 
             <h1
-                className={styles.profile__page__heading}    
+                className={styles.profile__page__heading}
             >
-                Please find the quotes you have added below
+                Your quotes in one place
             </h1>
 
-            {userQuotes?.map((quote) => (
-                <div 
-                    className={styles.quote__container} key={quote.id}
-                >
-                    <h1>{quote.author}</h1>
-                    <p 
-                        className={styles.quote__container__quote__text}
+            <div
+                className={styles.quotes__container} 
+            >
+                {userQuotes?.map((quote) => (
+                    <div
+                        className={styles.quote__wrapper} 
+                        key={quote.id}
                     >
-                        {quote.text}
-                    </p>
-                    <h4 
-                        className={styles.quote__container__category}
-                    >
-                        {quote.category}
-                    </h4>
-                    <Link to={`/quotes/${quote.id}`}>Modify or Delete</Link>
-                </div>
-            ))}
+                        <h1
+                            className={styles.quote__wrapper__quote__author}
+                        >
+                            {quote.author}
+                        </h1>
+                        <p
+                            className={styles.quote__wrapper__quote__text}
+                        >
+                            {quote.text}
+                        </p>
+                        <h4
+                            className={styles.quote__wrapper__category}
+                        >
+                            {quote.category}
+                        </h4>
+                        <Link 
+                            to={`/quotes/${quote.id}`}
+                        >
+                            Review
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
