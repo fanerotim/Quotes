93271dcdabd0c93f8dfc5a4266f8982d6cd95a4b
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-01 17:27:26 +0200

    fix a bug whereby user could have clicked on checkbox (react icon in my case) and break the text in the custom select
    conditional check fixes this issue

--- client/vite-project/src/hooks/useCustomSelect.js
+++ client/vite-project/src/hooks/useCustomSelect.js
@@ -10,15 +10,17 @@ const useCustomSelect = () => {
     }
 
     const updateChoice = (e) => {
-        const selectedNode = e.target.nodeName;
+        const clickedNode = e.target.nodeName;
 
-        // if the selected node is different than paragraph return
-        if (selectedNode !== 'P') {
-            return;
-        }   
-
+        // if the selected node is different than paragraph (text) or div (text container) return
+        // i.e. if user clicks on the checkox return. if allowed, this breaks the text in the custom select as checkbox does not have textContent
+        if (clickedNode !== 'P' && clickedNode !== 'DIV') {
+            return
+        }
+        
         // get the text of the new choice 
         const choiceText = e.target.textContent;
+
         // update choice, so it reflects in the custom select
         setChoice((prev) => choiceText);
         // close dropdown after selecting an option
