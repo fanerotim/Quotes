b08ff7d8c1befa812d6448b85829c8b571221edf
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-04 22:30:35 +0200

    update hardcoded url with a dynamic env variable

--- client/vite-project/src/hooks/useRegisterRequest.js
+++ client/vite-project/src/hooks/useRegisterRequest.js
@@ -4,7 +4,7 @@ const useRegisterRequest = () => {
 
     const register = async (values) => {
         try {
-            const token = await http.post('http://localhost:3000/user/register', { email: values.email, password: values.password });
+            const token = await http.post(`${import.meta.env.VITE_USER_URL}/register`, { email: values.email, password: values.password });
             return token;
         } catch(err) {
             throw err;
