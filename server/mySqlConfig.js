35cd7a7855237a1a30b241e5b408a5a4f9ceed79
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-05 20:57:41 +0200

    update mysql config, so that config details are passed based on NODE_ENV variable

--- server/mySqlConfig.js
+++ server/mySqlConfig.js
@@ -5,19 +5,32 @@ dotenv.config();
 let db = null;
 
 const mysqlConfig = () => {
-    
+
+    const environmentType = process.env.NODE_ENV;
+
+    const dbConfig = {
+        'development': {
+            host: process.env.DEV_HOST,
+            port: process.env.DEV_DB_PORT,
+            user: process.env.DEV_USER,
+            password: process.env.DEV_PASSWORD,
+            database: process.env.DEV_DATABASE,
+        },
+        'production': {
+            host: process.env.PROD_HOST,
+            port: process.env.PROD_DB_PORT,
+            user: process.env.PROD_USER,
+            password: process.env.PROD_PASSWORD,
+            database: process.env.PROD_DATABASE,
+        }
+    }
+
     if (db) {
         return db;
     }
 
-    db = mysql.createConnection({
-        host: process.env.HOST,
-        port: process.env.DB_PORT,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-    })
-    return db;  
+    db = mysql.createConnection(dbConfig[environmentType])
+    return db;
 }
 
 module.exports = mysqlConfig;
