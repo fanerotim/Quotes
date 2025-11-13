c181a7ef25fdd0d8263b10838a50ff3d7726b8c5
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-13 14:25:59 +0200

    change styles of session expired modal and make sure page cannot be scrolled vertically when modal is open
    reduce border-radius of container
    give some margin to secondary text to make it more readable
    import global styles and update submit button's styles to the global ones

--- client/vite-project/src/components/session-expired/SessionExpired.jsx
+++ client/vite-project/src/components/session-expired/SessionExpired.jsx
@@ -1,11 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../hooks/useAuthContext'
 import styles from './SessionExpired.module.scss'
+import globalStyles from '../../App.module.scss';
 
 const SessionExpired = () => {
 
     const { dispatch } = useAuthContext();
     const navigate = useNavigate();
+
+    // make sure page cannot be scrolled when modal pops up
+    const bodyElement = document.querySelector('body');
+    bodyElement.style.overflowY = 'hidden';
 
     const handleClick = () => {
         // dispatch LOGOUT event;
@@ -33,7 +38,7 @@ const SessionExpired = () => {
                 </p>
                 <button
                     onClick={handleClick}
-                    className={styles.session__expired__button}
+                    className={globalStyles.submit__button}
                 >
                     Login again
                 </button>
