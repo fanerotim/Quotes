1c54c05eb1647e66fcd25a056d2fa01855f4ad5b
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-13 16:55:47 +0200

    adjust styles of quote cards on user-profile page
    review btn is slightly different, so could not inherit global .submit__button styles, so reduced its border-radius and font-size in attempt to make it more visually pleasing experience
    
    also reduce font-size of paragraph indicating author

--- client/vite-project/src/components/user-profile/UserProfile.jsx
+++ client/vite-project/src/components/user-profile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import styles from './UserProfile.module.scss'
+import styles from './UserProfile.module.scss';
 import UpdatePassword from './update-password/UpdatePassword';
 import { Link } from 'react-router-dom';
 import useGetUserQuotes from '../../hooks/useGetUserQuotes';
