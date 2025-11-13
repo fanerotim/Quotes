b09f577090f427a0c6b04721faaf4a07a31e2a9f
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-13 15:15:44 +0200

    set global submit button styles to load-more-quotes button

--- client/vite-project/src/components/load-more-quotes/LoadMoreQuotes.jsx
+++ client/vite-project/src/components/load-more-quotes/LoadMoreQuotes.jsx
@@ -1,4 +1,6 @@
+// i am not using any custom styles for this button, but keeping the import here in case i need add specific styles later on
 import styles from './LoadMoreQuotes.module.scss'
+import globalStyles from '../../App.module.scss';
 import useLoadMoreQuotes from '../../hooks/useLoadMoreQuotes';
 import useHasMoreQuotes from '../../hooks/useHasMoreQuotes';
 
@@ -14,7 +16,7 @@ const LoadMoreQuotes = () => {
         <>
         {getHasMoreStatus() && <button
             onClick={clickHandler}
-            className={styles.load__more__btn}>
+            className={globalStyles.submit__button}>
             Load More
         </button>}
         </>
