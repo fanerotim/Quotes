aa24420d7aa48ca00c1d3c13cfdae3b3c07d3511
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-15 17:15:25 +0200

    place form state components / messages on reset-password page
    import error toaster, success modal and loader
    render them conditionally based on user input / server response
    place them on the grid and wrap each component in a html element / div in this case

--- client/vite-project/src/components/reset-password/ResetPassword.jsx
+++ client/vite-project/src/components/reset-password/ResetPassword.jsx
@@ -2,6 +2,9 @@ import styles from './ResetPassword.module.scss';
 import globalStyles from '../../App.module.scss';
 import useResetPassword from '../../hooks/useResetPassword';
 import useForm from '../../hooks/useForm';
+import Toaster from '../toaster/Toaster';
+import SuccessModal from '../success-modal/SuccessModal';
+import Loader from '../loader/Loader';
 
 const initialValues = {
     email: ''
@@ -38,42 +41,64 @@ const ResetPassword = () => {
                 </h2>
             </div>
 
-            {!isSubmitted &&
+            {isSubmitted
 
-                <form
-                    onSubmit={(e) => handleSubmit(e, values)}
-                    className={styles.reset__password__form}
+                ?
+                <div
+                    className={styles.success__modal__container}
                 >
+                    <SuccessModal />
+                </div>
+                :
+
+                isLoading
+                    ?
 
                     <div
-                        className={styles.form__data}
+                        className={styles.loader__container}
                     >
-                        <label
-                            className={styles.reset__password__form__label}
-                        >
-                            Email address
-                        </label>
-                        <input
-                            onChange={handleChange}
-                            className={styles.reset__password__form__input}
-                            value={values.email}
-                            placeholder='Enter your email'
-                            name='email'
-                            type="email"
-                            required
-                        />
+                        <Loader />
                     </div>
+                    :
 
-                    <button
-                        className={globalStyles.submit__button}
-                        disabled={isLoading}
+                    <form
+                        onSubmit={(e) => handleSubmit(e, values)}
+                        className={styles.reset__password__form}
                     >
-                        Submit
-                    </button>
-                </form>}
 
-            {error && <p>{error}</p>}
-            {isSubmitted && <h2>Password updated successfully. Please check your email and log in again.</h2>}
+                        <div
+                            className={styles.form__data}
+                        >
+                            <label
+                                className={styles.reset__password__form__label}
+                            >
+                                Email address
+                            </label>
+                            <input
+                                onChange={handleChange}
+                                className={styles.reset__password__form__input}
+                                value={values.email}
+                                placeholder='Enter your email'
+                                name='email'
+                                type="email"
+                                required
+                            />
+                        </div>
+
+                        <button
+                            className={globalStyles.submit__button}
+                            disabled={isLoading}
+                        >
+                            Submit
+                        </button>
+                    </form>}
+
+            <div
+                className={styles.error__toaster__container}
+            >
+                <Toaster message={error} />
+            </div>
+
         </section>
     )
 }
