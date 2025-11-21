67fc9fd86d01ee395509a59ad5f7c01df2309863
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-21 14:29:42 +0200

    add basic design and styles to NotFound component as it was totally left behind / completely unstyled

--- client/vite-project/src/components/not-found/NotFound.jsx
+++ client/vite-project/src/components/not-found/NotFound.jsx
@@ -1,8 +1,25 @@
+import styles from '../not-found/NotFound.module.scss';
+import globalStyles from '../../App.module.scss';
+import {Link} from 'react-router-dom';
+
 const NotFound = () => {
     return (
-        <>
-            <h1>Not Found</h1>
-        </>
+        <section
+            className={styles.not__found__page__wrapper}
+        >
+            <h1
+                className={styles.not__found__page__text}
+            >
+                Not Found
+            </h1>
+
+            <Link
+                to={'/'}
+                className={globalStyles.submit__button}
+            >
+                Home
+            </Link>
+        </section>
     )
 }
 
