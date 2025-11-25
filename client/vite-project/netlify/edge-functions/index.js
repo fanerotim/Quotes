891ab088cae474b83de3de42d7e3ae33bcb61fde
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-25 15:02:05 +0200

    first test to rewrite requests by the facebook bot

--- client/vite-project/netlify/edge-functions/index.js
+++ client/vite-project/netlify/edge-functions/index.js
@@ -1,13 +1,7 @@
 export default async (req, context) => {
-    console.log('THOSE ARE THE HEADERS AND FIRST LOG - SERVES AS GENERAL INFORMATION.', req.headers);
-
-    console.log('Context provides access to the pathname ', context.url.pathname)
-    console.log('this pointed to Window obj, but it was empty, so lets print it now to double check: ', window)
 
     const url = new URL(`${context.url.pathname}`, 'https://quotes-117d.onrender.com/quotes');
-    console.log('CONSTRUCTED URL: ', url);
-
-    new Response('Testing my first edge function')
+    new url;
 };
 
 export const config = {
