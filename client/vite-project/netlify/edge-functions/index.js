f6d2e41a19490b95c0f3d645ab6e4b2b102b9dbe
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-25 14:53:44 +0200

    context seems to provide access to pathname. logging it and testing a dynamically constructed url.
    it's bad having to debug here, but I need to test facebook's bot behavior and do not have a staging environment for this app, as it's a portfolio project really

--- client/vite-project/netlify/edge-functions/index.js
+++ client/vite-project/netlify/edge-functions/index.js
@@ -1,8 +1,12 @@
 export default async (req, context) => {
     console.log('THOSE ARE THE HEADERS AND FIRST LOG - SERVES AS GENERAL INFORMATION.', req.headers);
-    console.log('THIS IS THE REQUEST - SERVES AS GENERAL INFORMATION.', req);
-    console.log('let`s check the context object: ', context)
-    console.log('I am trying to log only the pathname of the requested url...', window)
+
+    console.log('Context provides access to the pathname ', context.url.pathname)
+    console.log('this pointed to Window obj, but it was empty, so lets print it now to double check: ', window)
+
+    const url = new URL(`${context.url.pathname}`, `${import.meta.env.VITE_BASE_URL}`);
+    console.log('CONSTRUCTED URL: ', url);
+
     new Response('Testing my first edge function')
 };
 
