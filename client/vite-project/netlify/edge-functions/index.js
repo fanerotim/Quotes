f2fbc8ce85fca13a78e0ed2ecc7b453563b1323f
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-26 20:12:26 +0200

    Update index.js

--- client/vite-project/netlify/edge-functions/index.js
+++ client/vite-project/netlify/edge-functions/index.js
@@ -1,18 +1,18 @@
-// export default async (req, context) => {
+export default async (req, context) => {
 
-//     console.log(req, 'THIS IS THE ORIGINAL REQUEST')
-//     console.log(context, 'AND THIS IS THE CONTEXT')
+    console.log(req, 'THIS IS THE ORIGINAL REQUEST')
+    console.log(context, 'AND THIS IS THE CONTEXT')
 
-//     // keeping the url hardcoded for now as initially this file was not able to read the env variables
-//     const url = new URL(`${context.url.pathname}`, 'https://quotes-117d.onrender.com/quotes');
-//     return Response.redirect(url);
-// };
+    // keeping the url hardcoded for now as initially this file was not able to read the env variables
+    const url = new URL(`${context.url.pathname}`, 'https://quotes-117d.onrender.com/quotes');
+    return Response.redirect(url);
+};
 
-// export const config = {
-//     path: '/*',
-//     excludedPath: '/robots.txt',
-//     // this solutions works. it detects the facebookbot, so we can redirect based on this condition
-//     header: {
-//         'user-agent': '(facebookexternalhit)'
-//     }
-// }
+export const config = {
+    // path: '/*',
+    // excludedPath: '/robots.txt',
+    // // this solutions works. it detects the facebookbot, so we can redirect based on this condition
+    // header: {
+    //     'user-agent': '(facebookexternalhit)'
+    // }
+}
