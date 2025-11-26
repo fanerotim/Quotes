c2e72d2f329a2bd8b67a81cd36e6f631c3b24e1b
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-26 17:06:44 +0200

    Update index.js

--- client/vite-project/netlify/edge-functions/index.js
+++ client/vite-project/netlify/edge-functions/index.js
@@ -1,4 +1,9 @@
 export default async (req, context) => {
+
+    if (context.pathname.includes('robots')) {
+        context.next();
+    }
+    
     // keeping the url hardcoded for now as initially this file was not able to read the env variables
     const url = new URL(`${context.url.pathname}`, 'https://quotes-117d.onrender.com/quotes');
     console.log(url)
