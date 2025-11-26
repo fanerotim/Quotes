7a4a4febe02cb3e891e96775d98fe29ef91a25d9
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-11-26 15:47:37 +0200

    Update botController.js

--- server/controllers/botController.js
+++ server/controllers/botController.js
@@ -3,9 +3,10 @@ const quoteService = require('../services/quoteService');
 
 router.get('/:id', async (req, res, next) => {
     const { id } = req.params;
-
+    console.log('these are req.params', req.params)
     try {
         const result = await quoteService.getQuote(id);
+        console.log('this is result', result)
         const quoteDetails = result[0];
         res.render('bot', { quoteDetails });
     } catch (err) {
