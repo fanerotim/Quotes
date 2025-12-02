2fb08845c0f7411f4e3b60c68b454e67fe365852
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-12-02 18:13:43 +0200

    add Facebot to the list of bots that will be redirected to backend by Netlify's edge function

--- server/utils/createDynamicSvg.js
+++ server/utils/createDynamicSvg.js
@@ -2,16 +2,20 @@ const { promises } = require('fs');
 const { join } = require('path');
 
 const createDynamicSvg = async (text) => {
-    console.log('I was called to create a dynamic svg with this text', text);
-    const content = `
-    <svg viewBox="0 0 1200 250" xmlns="http://www.w3.org/2000/svg">
-        <style>
-            .small {
-            font: italic 5px sans-serif;
-            }
 
-        </style>
-        <text x="20" y="35" class="small">${text}</text>
+    const content = `
+    <svg 
+        viewBox="0 0 1200 250"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        
+        <text 
+            x="150" 
+            y="75"
+            font-size="15px"
+        >
+            ${text}
+        </text>
     </svg>`
 
     try {
