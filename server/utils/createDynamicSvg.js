49c91dd28240b82e518da11d8ae9a981e050a798
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-12-02 20:55:17 +0200

    changed text position on the svg for testing purposes

--- server/utils/createDynamicSvg.js
+++ server/utils/createDynamicSvg.js
@@ -5,13 +5,16 @@ const createDynamicSvg = async (text) => {
 
     const content = `
     <svg 
-        viewBox="0 0 1200 250"
+        viewBox="0 0 1200 150"
+        height="150"
+        width="600"
+        style="background-color:green"
         xmlns="http://www.w3.org/2000/svg"
     >
         
-        <text 
-            x="150" 
-            y="75"
+        <text
+            x="5"
+            y="30" 
             font-size="15px"
         >
             ${text}
