2855e6203e3159ee46d43fbce51b1d4f80c681fc
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-12-03 17:30:57 +0200

    Update createDynamicSvg.js

--- server/utils/createDynamicSvg.js
+++ server/utils/createDynamicSvg.js
@@ -5,23 +5,23 @@ const createDynamicSvg = async (text, author) => {
 
     const content = `
     <svg 
-        viewBox="0 0 300 150"
+        viewBox="0 0 150 100"
+        width="150"
+        height="100"
         xmlns="http://www.w3.org/2000/svg"
     >
         <style>
 
             .text {
-                font-size: 5px;
+                font-size: 10px;
                 fill: rgba(40, 40, 40, 0.8);
             }
 
         </style>
 
         <text
-            x="50"
+            x="10"
             y="35"
-            dy="55"
-            dx="50" 
             class="text"
         >
             ${author}
