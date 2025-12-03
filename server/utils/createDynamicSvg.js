8348a3e070c8cc1fcfde04cf1854bd389818d74e
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-12-03 12:52:22 +0200

    add changes to svg text to test caching

--- server/utils/createDynamicSvg.js
+++ server/utils/createDynamicSvg.js
@@ -6,16 +6,23 @@ const createDynamicSvg = async (text) => {
     const content = `
     <svg 
         viewBox="0 0 1200 150"
+        xmlns="http://www.w3.org/2000/svg"
         height="150"
         width="600"
-        style="background-color:green"
-        xmlns="http://www.w3.org/2000/svg"
     >
-        
+        <style>
+
+            .text {
+                font-size: 25px;
+                fill: rgba(17, 179, 84, 0.8);
+            }
+
+        </style>
+
         <text
             x="5"
             y="30" 
-            font-size="15px"
+            class="text"
         >
             ${text}
         </text>
