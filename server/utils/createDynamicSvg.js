af544bf803441b648e4abe7ded6fe4c2696bcfa7
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-12-06 16:09:25 +0200

    clean up createDynamicSvg fn and move svg markup generaton in generateSvgMarkup fn

--- server/utils/createDynamicSvg.js
+++ server/utils/createDynamicSvg.js
@@ -5,62 +5,12 @@ const { generateSvgMarkup } = require('./generateSvgMarkup');
 
 const createDynamicSvg = async (text, author) => {
 
-    const outputText = prepareOgImageText(text);
-    const lines = generateSvgMarkup(outputText);
-
-    let content = `
-    <svg 
-        width="1200"
-        height="630"
-        viewBox="0 0 1200 630"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <style>
-
-            .text {
-                font-size: 40px;
-                font-weight: 600;
-                font-variant: small-caps;
-                fill: rgba(0, 0, 0, 0.8);
-                background: "pink";
-            }
-
-        </style>
-
-        <rect width="1200" height="630" fill="rgba(200, 200, 200, 0.7)" />
-
-        <text
-            x="5%"
-            y="140px"
-            class="text"
-        >
-            ${outputText[0]}
-        <tspan 
-            fill="red"
-            font-size="110px"
-            x="3%"
-            y="100px"
-        >
-            “
-        </tspan>
-
-        </text>
-
-        ${[...lines]}
-
-        <text
-            x="5%"
-            y="580px"
-            font-size="28px"
-            font-weight="700"
-            fill="rgba(27, 27, 27, 0.7)"
-        >
-          - ${author}
-        </text>
-    </svg>`
+    const ogImageTextArray = prepareOgImageText(text);
+    const svgArray = generateSvgMarkup(ogImageTextArray, author);
+    const svg = svgArray.join(' ');
 
     try {
-        await promises.writeFile(join(__dirname, '../views/quoteOgImage.svg'), content);
+        await promises.writeFile(join(__dirname, '../views/quoteOgImage.svg'), svg);
         return { message: 'Svg file was written / created successfully!' }
     } catch (err) {
         console.log(err);
--- server/utils/generateSvgMarkup.js
+++ server/utils/generateSvgMarkup.js
@@ -1,17 +1,71 @@
-const generateSvgMarkup = (textArr) => {
+const generateSvgMarkup = (textArr, author) => {
     const svg = [];
 
-    for (let i = 0; i < textArr.length - 1; i++) {
+    const firstLineOfText = `
+    <svg 
+        width="1200"
+        height="630"
+        viewBox="0 0 1200 630"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <style>
+
+            .text {
+                font-size: 40px;
+                font-weight: 600;
+                font-variant: small-caps;
+                fill: rgba(0, 0, 0, 0.8);
+                background: "pink";
+            }
+
+        </style>
+
+        <rect width="1200" height="630" fill="rgba(200, 200, 200, 0.7)" />
+
+        <text
+            x="8%"
+            y="140px"
+            class="text"
+        >
+            ${textArr.shift()}
+        <tspan 
+            fill="red"
+            font-size="110px"
+            x="3%"
+            y="140px"
+        >
+            “
+        </tspan>
+
+        </text>`
+
+    svg.push(firstLineOfText);
+
+    for (let i = 0; i < textArr.length; i++) {
         svg.push(`
         <text 
-            x="5%" 
+            x="8%" 
             y="140px" 
-            dy="${60 * (i + 1)}px" 
+            dy="${55 * (i + 1)}px" 
             class="text">
-            ${textArr[i + 1]}
+            ${textArr[i]}
         </text>`
         )
     }
+
+    const lastLineOfText = `
+        <text
+            x="8%"
+            y="580px"
+            font-size="28px"
+            font-weight="700"
+            fill="rgba(27, 27, 27, 0.7)"
+        >
+          - ${author}
+        </text>
+    </svg>
+    `
+    svg.push(lastLineOfText);
     return svg;
 }
 
